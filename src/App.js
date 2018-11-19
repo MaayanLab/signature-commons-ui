751dcab9ee9149c21d8998bf2351e56a16b19870
Daniel J. B. Clarke <u8sand@gmail.com>
2018-11-19 17:01:45 -0500

    Updated App to incorporate demos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Admin from './Admin';
+import DBCK from './DBCK';
+import Home from './Home';
+import Query from './Query';
+import Stats from './Stats';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/query">Query</Link>
+        </li>
+        <li>
+          <Link to="/stats">Stats</Link>
+        </li>
+        <li>
+          <Link to="/dbck">Database Check</Link>
+        </li>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+      </ul>
+
+      <Route exact path="/" component={Home} />
+      <Route exact path="/query" component={Query} />
+      <Route exact path="/stats" component={Stats} />
+      <Route exact path="/dbck" component={DBCK} />
+      <Route exact path="/admin" component={Admin} />
+    </div>
+  </Router>
+)
 
 export default App;
---
+++ src/Home/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    This is a demo.
+  </div>
+);
+
+export default Home
