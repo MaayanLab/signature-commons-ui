5085172ed594935cd2af7bc4a7cfa50c1bd88b97
Daniel J. B. Clarke <u8sand@gmail.com>
2018-11-30 11:35:09 -0500

    Setup materializecss for nicer official looking site

--- src/Admin/loopback-provider.js
+++ src/Admin/loopback-provider.js
@@ -111,7 +111,7 @@ export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
      * @returns {Object} Data response
      */
     const convertHTTPResponse = (response, type, resource, params) => {
-        const { headers, json } = response;
+        const { json } = response;
         switch (type) {
             case GET_LIST:
             case GET_MANY_REFERENCE:
--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Home from './Home';
 import TermQuery from './TermQuery';
 import ArbitraryQuery from './ArbitraryQuery';
 import Stats from './Stats';
+import Values from './Values';
 
 const App = () => (
   <Router>
-    <div>
-      <ul>
+    <div className="root">
+      {/* <ul>
         <li>
           <Link to="/signature-commons-ui">Home</Link>
         </li>
@@ -24,17 +25,21 @@ const App = () => (
           <Link to="/signature-commons-ui/stats">Stats</Link>
         </li>
         <li>
+          <Link to="/signature-commons-ui/values">Values</Link>
+        </li>
+        <li>
           <Link to="/signature-commons-ui/dbck">Database Check</Link>
         </li>
         <li>
           <Link to="/signature-commons-ui/admin">Admin</Link>
         </li>
-      </ul>
+      </ul> */}
 
       <Route exact path="/signature-commons-ui/" component={Home} />
       <Route exact path="/signature-commons-ui/term-query" component={TermQuery} />
       <Route exact path="/signature-commons-ui/arbitrary-query" component={ArbitraryQuery} />
       <Route exact path="/signature-commons-ui/stats" component={Stats} />
+      <Route exact path="/signature-commons-ui/values" component={Values} />
       <Route exact path="/signature-commons-ui/dbck" component={DBCK} />
       <Route exact path="/signature-commons-ui/admin" component={Admin} />
     </div>
