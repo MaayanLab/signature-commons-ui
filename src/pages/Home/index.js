36796b181b4983c951a84c8e05b86360a4d6cf72
Daniel J. B. Clarke <u8sand@gmail.com>
2019-01-11 13:21:42 -0500

    Added github buttons to footer
    Fixes #48

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -64,7 +64,12 @@ const Footer = () => (
   <footer className="page-footer grey lighten-3 black-text">
     <div className="container">
       <div className="row">
-        <div className="col offset-l10 offset-m6 l2 m6 s12">
+        <div className="col l4 m6 s12">
+          <a className="github-button" href="https://github.com/dcic/signature-commons-ui" data-size="large" aria-label="View Source Code dcic/signature-commons-ui on GitHub">View Source Code</a>
+          <a className="github-button" href="https://github.com/dcic/signature-commons-ui/issues" data-size="large" aria-label="Submit Bug Report dcic/signature-commons-ui on GitHub">Submit Bug Report</a>
+        </div>
+
+        <div className="col offset-l6 l2 m6 s12">
           <img src="https://amp.pharm.mssm.edu/enrichmentapi/images/dcic.png" alt="BD2K-LINCS Data Coordination and Integration Center" height="130" /><br />
         </div>
       </div>
