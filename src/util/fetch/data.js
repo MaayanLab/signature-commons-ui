94a0b8c149513825e0c2b6a7a7921cd3d5c71e79
Daniel J. B. Clarke <u8sand@gmail.com>
2019-01-14 16:05:14 -0500

    Hack: Return empty results when we get nothing from data API

--- src/util/fetch/data.js
+++ src/util/fetch/data.js
@@ -1,16 +1,20 @@
 const base_url = 'https://amp.pharm.mssm.edu/enrichmentapi/api'
 
 export async function fetch_data(endpoint, body, signal) {
-  return JSON.parse(
-    await (
-      await fetch(
-        base_url + endpoint,
-        {
-          method: 'POST',
-          body: JSON.stringify(body),
-          signal: signal,
-        }
-      )
-    ).text()
-  )
+  try {
+    return JSON.parse(
+      await (
+        await fetch(
+          base_url + endpoint,
+          {
+            method: 'POST',
+            body: JSON.stringify(body),
+            signal: signal,
+          }
+        )
+      ).text()
+    )
+  } catch(e) {
+    return {"signatures": [], "matchingEntities": [], "queryTimeSec": 0, "results": {}}
+  }
 }
