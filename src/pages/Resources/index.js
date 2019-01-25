fbd6feb040734ab589402db074dfe7da0a76c3c5
Daniel J. B. Clarke <u8sand@gmail.com>
2019-01-25 11:47:19 -0500

    Added placeholder buttons for downloading libraries
    References #70

--- src/pages/Resources/index.js
+++ src/pages/Resources/index.js
@@ -4,6 +4,7 @@ import { ShowMeta } from '../../components/ShowMeta';
 import { fetch_meta_post } from '../../util/fetch/meta';
 import { Label } from '../../components/Label';
 import M from "materialize-css";
+import { call } from '../../util/call';
 
 export const primary_resources = [
   'CREEDS',
@@ -49,6 +50,10 @@ export default class Test extends React.PureComponent {
       resources: [],
       selected: null,
     }
+
+    this.download = this.download.bind(this)
+    this.addToCart = this.addToCart.bind(this)
+    this.removeFromCart = this.removeFromCart.bind(this)
   }
   async componentDidMount() {
     const {response: libraries} = await fetch_meta_post('/libraries/find', {})
@@ -70,6 +75,22 @@ export default class Test extends React.PureComponent {
     this.setState({
       resources: Object.values(resources),
     })
+  }
+
+  async download(library_id) {
+    alert('coming soon')
+  }
+
+  addToCart(id) {
+    this.props.updateCart(
+      this.props.cart.add(id)
+    )
+  }
+
+  removeFromCart(id) {
+    this.props.updateCart(
+      this.props.cart.delete(id)
+    )
   }
 
   render() {
@@ -95,25 +116,61 @@ export default class Test extends React.PureComponent {
                       key={library.id}
                     >
                       <div
-                        className="collapsible-header"
+                        className="page-header"
                         style={{
                           padding: 10,
                           display: 'flex',
-                          flexDirection: 'row',
+                          flexDirection: "column",
                           backgroundColor: 'rgba(255,255,255,1)',
                         }}
                       >
-                        <Label
-                          item={library}
-                          visibility={1}
-                        />
-                        <div style={{ flex: '1 0 auto' }}>&nbsp;</div>
-                        <a
-                          href="#!"
-                          style={{ border: 0 }}
-                        >
-                          <i className="material-icons">expand_more</i>
-                        </a>
+                        <div
+                          style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                        }}>
+                          <Label
+                            item={library}
+                            visibility={1}
+                          />
+                        </div>
+                        <div style={{
+                          display: 'flex',
+                          flexDirection: "row",
+                        }}>
+                          <IconButton
+                            alt="Signature Commons"
+                            img="favicon.ico"
+                          />
+                          &nbsp;
+                          <IconButton
+                            alt="Download"
+                            icon="file_download"
+                            onClick={call(this.download, library.id)}
+                          />
+                          &nbsp;
+                          {this.props.cart.has(library.id) ? (
+                            <IconButton
+                              alt="Remove from Cart"
+                              icon="remove_shopping_cart"
+                              onClick={call(this.removeFromCart, library.id)}
+                            />
+                          ) : (
+                            <IconButton
+                              alt="Add to Cart"
+                              icon="add_shopping_cart"
+                              onClick={call(this.addToCart, library.id)}
+                            />
+                          )}
+                          <div style={{ flex: '1 0 auto' }}>&nbsp;</div>
+                          <a
+                            href="#!"
+                            className="collapsible-header"
+                            style={{ border: 0 }}
+                          >
+                            <i className="material-icons">expand_more</i>
+                          </a>
+                        </div>
                       </div>
                       <div
                         className="collapsible-body"
