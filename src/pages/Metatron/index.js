dafca0b5e415874772ff784768157b171acbf26a
John Erol Evangelista <je.evangelista04@gmail.com>
2019-02-01 17:46:27 -0500

    Cleaned library filtering.

--- src/pages/Metatron/index.js
+++ src/pages/Metatron/index.js
@@ -1,186 +1,193 @@
 import React from "react";
 import { Admin,
-         AutocompleteInput,
          Datagrid,
          Filter,
+         LinearProgress,
          List,
          ReferenceField,
          ReferenceInput,
          Resource,
          SelectInput,
-         UrlField,
          TextField } from 'react-admin';
 import { base_url, fetch_meta } from '../../util/fetch/meta';
 import loopbackProvider from '../Admin/loopback-provider';
-import filterHeaders from './filter-headers';
+import { PostFilter } from './signaturehelper';
 
 const dataProvider = loopbackProvider(base_url);
-const dataProviderMod = filterHeaders(dataProvider)
 
-class Metatron extends React.Component {
+class Metatron extends React.PureComponent {
   constructor(props) {
     super(props)
 
     this.state = {
       LibraryList: null,
-      SignatureList: null,
       EntityList: null,
-      LibSelected: null,
+      SignatureList: null,
+      library_stats: null,
+      entity_stats: null,
+      signature_stats: null,
+      LibNum: 140,
     }
     this.filterHandler = this.filterHandler.bind(this);
+    this.LibraryList = this.LibraryList.bind(this);
+    this.EntityList = this.EntityList.bind(this);
+    this.SignatureList = this.SignatureList.bind(this);
   }
 
-  filterHandler(e){
-    (async () => {
+  LibraryList(props) {
+    return (
+      <List {...props}>
+        <Datagrid>
+          <TextField
+            source="id"
+          />
+          {Object.keys(this.state.library_stats).map((k) => (
+            <TextField
+              key={k}
+              source={"meta." + k}
+            />
+          ))}
+        </Datagrid>
+      </List>
+    )
+  }
+
+  SignatureList(props){
+    return(
+      <List
+        {...props}
+        filters={
+          <PostFilter
+            libnum={this.state.LibNum}
+            filterhandler={this.filterHandler}
+          />
+        }
+        filterDefaultValues={{"library": "308de661-d3e2-11e8-8fe6-787b8ad942f3"}}
+      > 
+        <Datagrid>
+          <TextField
+            source="id"
+          />
+          <ReferenceField source="library" reference="libraries">
+            <TextField source="meta.Library name" />
+          </ReferenceField>
+          {Object.keys(this.state.signature_stats).map((k) => (
+            <TextField
+              key={k}
+              label={k}
+              source={"meta." + k}
+            />
+          ))}
+        </Datagrid> 
+      </List>
+    )
+  }
+
+  EntityList(props) {
+    return (
+      <List {...props}>
+        <Datagrid>
+          <TextField
+            source="id"
+          />
+          {Object.keys(this.state.entity_stats).map((k) => (
+            <TextField
+              key={k}
+              source={"meta." + k}
+            />
+          ))}
+        </Datagrid>
+      </List>
+    )
+  }
+
+  // get_signatures(signature_stats) {
+  //   if(this.state.LibNum){
+  //     this.setState({
+  //         SignatureList:<Datagrid>
+  //                         <TextField
+  //                           source="id"
+  //                         />
+  //                         <ReferenceField source="library" reference="libraries">
+  //                           <TextField source="meta.Library name" />
+  //                         </ReferenceField>
+  //                         {Object.keys(signature_stats).map((k) => (
+  //                           <TextField
+  //                             key={k}
+  //                             label={k}
+  //                             source={"meta." + k}
+  //                           />
+  //                         ))}
+  //                       </Datagrid>
+  //       })
+  //   }
+  // }
+
+  async filterHandler(e){
+      // console.log(e)
+      // this.setState({
+      //   SignatureList: <LinearProgress />
+      // }, async () =>{
+      //   const uid = Object.values(e).slice(0,36).join('')
+      //   const { response: signature_stats} = await fetch_meta('/signatures/key_count?filter={"where":{"library":"'+uid+'"}}')
+      //   this.get_signatures(signature_stats)
+      // });
       const uid = Object.values(e).slice(0,36).join('')
-      const { response: signature_stats } = await fetch_meta('/signatures/key_count?filter={"where":{"library":"'+uid+'"}}')
-      const { response: library_stats } = await fetch_meta('/libraries/key_count')
-      const PostFilter = (props) => (
-          <Filter {...props}>
-              <ReferenceInput label="Library" source="library" reference="libraries" perPage={library_stats.$validator} onChange={this.filterHandler} allowEmpty alwaysOn>
-                  <SelectInput optionText="meta.Library name"/>
-              </ReferenceInput>
-          </Filter>
-      );
+      const { response: signature_stats} = await fetch_meta('/signatures/key_count?filter={"where":{"library":"'+uid+'"}}')
       this.setState({
-        SignatureList: (props) => (
-          <List {...props} filters={<PostFilter />}>
-            <Datagrid>
-              <TextField
-                source="id"
-              />
-              <ReferenceField source="library" reference="libraries">
-                <TextField source="meta.Library name" />
-              </ReferenceField>
-              {Object.keys(signature_stats).map(function(k){
-                return(
-                  <TextField
-                    key={k}
-                    label={k}
-                    source={"meta." + k}
-                  />
-                );
-              })}
-            </Datagrid>
-          </List>
-        )
-      })
-    })();
+        // signature_stats: signature_stats,
+        signature_stats: signature_stats,
+      });
   }
 
   componentDidMount() {
-    const IDField = function({ record = {} }){
-      const link = '/metatron#/signatures?filter={"library"%3A"'+ record.id +'"%7D&order=DESC&page=1&perPage=10&sort=id'
-      return(<a href={link}>{record.id}</a>);
-    };
-    IDField.defaultProps = { label: 'ID' };
-
     (async () => {
       const { response: library_stats } = await fetch_meta('/libraries/key_count')
       this.setState({
-        LibraryList: (props) => (
-          <List {...props}>
-            <Datagrid>
-              <IDField
-                source="id"
-              />
-              <TextField
-                key="Library name"
-                label="Library name"
-                source={"meta.Library name"}
-              />
-              {Object.keys(library_stats).map(function(k){
-                if (k!=="Library name"){
-                return(
-                  <TextField
-                    key={k}
-                    label={k}
-                    source={"meta." + k}
-                  />
-                )
-              }
-              })}
-            </Datagrid>
-          </List>
-        )
+        LibNum: library_stats.$validator,
+        library_stats: library_stats,
       })
+      // this.get_libraries(library_stats)
     })();
     (async () => {
-      const { response: signature_stats } = await fetch_meta('/signatures/key_count')
-      const { response: library_stats } = await fetch_meta('/libraries/key_count')
-      const PostFilter = (props) => (
-          <Filter {...props}>
-              <ReferenceInput label="Library" source="library" reference="libraries" perPage={library_stats.$validator} onChange={this.filterHandler} allowEmpty alwaysOn>
-                  <SelectInput optionText="meta.Library name"/>
-              </ReferenceInput>
-          </Filter>
-      );
+      const uid = "308de661-d3e2-11e8-8fe6-787b8ad942f3"
+      const { response: signature_stats} = await fetch_meta('/signatures/key_count?filter={"where":{"library":"'+uid+'"}}')
+      const SigList = this.SigList
       this.setState({
-        SignatureList: (props) => (
-          <List {...props} filters={<PostFilter />}>
-            <Datagrid>
-              <TextField
-                source="id"
-              />
-              <ReferenceField source="library" reference="libraries">
-                <TextField source="meta.Library name" />
-              </ReferenceField>
-              {Object.keys(signature_stats).map(function(k){
-                return(
-                  <TextField
-                    key={k}
-                    label={k}
-                    source={"meta." + k}
-                  />
-                );
-              })}
-            </Datagrid>
-          </List>
-        )
+        // SignatureList: (props) => <SigList {...props} signature_stats={signature_stats} />,
+        signature_stats: signature_stats,
       })
+      // this.get_signatures(signature_stats)
     })();
     (async () => {
       const { response: entity_stats } = await fetch_meta('/entities/key_count')
       this.setState({
-        EntityList: (props) => (
-          <List {...props}>
-            <Datagrid>
-              <TextField
-                source="id"
-              />
-              {Object.keys(entity_stats).map((k) => (
-                <TextField
-                  key={k}
-                  label={k}
-                  source={"meta." + k}
-                />
-              ))}
-            </Datagrid>
-          </List>
-        )
+        // SignatureList: (props) => <SigList {...props} signature_stats={signature_stats} />,
+        entity_stats: entity_stats,
       })
+      // this.get_entities(entity_stats)
     })();
   }
 
   render() {
     return (
-      <Admin dataProvider={dataProviderMod}>
-        {this.state.LibraryList === null ? <div /> : (
+      <Admin dataProvider={dataProvider}>
+        {this.state.library_stats === null ? <div /> : (
           <Resource
             name="libraries"
-            list={this.state.LibraryList}
+            list={this.LibraryList}
           />
         )}
-        {this.state.SignatureList === null ? <div /> : (
+        {this.state.signature_stats === null ? <div /> : (
           <Resource
             name="signatures"
-            list={this.state.SignatureList}
+            list={this.SignatureList}
           />
         )}
-        {this.state.EntityList === null ? <div /> : (
+        {this.state.entity_stats === null ? <div /> : (
           <Resource
             name="entities"
-            list={this.state.EntityList}
+            list={this.EntityList}
           />
         )}
       </Admin>
---
+++ src/pages/Metatron/signaturehelper.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Filter,
+         List,
+         ReferenceInput,
+         SelectInput } from 'react-admin';
+
+export const PostFilter = (props) => (
+      <Filter {...props}>
+        <ReferenceInput label="Library"
+                        source="library"
+                        reference="libraries"
+                        onChange={props.filterhandler}
+                        perPage={props.libnum}
+                        sort={{ field: 'id', order: 'ASC' }}
+                        allowEmpty={false}
+                        alwaysOn
+                        >
+          <SelectInput optionText="meta.Library name"/>
+        </ReferenceInput>
+      </Filter>
+  )
