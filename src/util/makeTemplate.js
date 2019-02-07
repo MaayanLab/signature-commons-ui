5df5516e67df9b764d53bd0ccb7df56ae9585c45
Daniel J. B. Clarke <u8sand@gmail.com>
2019-02-07 11:50:20 -0500

    makeTemplate hotfix--replace spaces with `_`

--- src/util/makeTemplate.js
+++ src/util/makeTemplate.js
@@ -2,7 +2,7 @@ export function makeTemplate(
   templateString,
   templateVariables
 ) {
-  const keys = Object.keys(templateVariables);
+  const keys = Object.keys(templateVariables).map((key) => key.replace(' ', '_'))
   const values = Object.values(templateVariables);
   let templateFunction = new Function(...keys, `return \`${templateString}\`;`);
   return templateFunction(...values);
