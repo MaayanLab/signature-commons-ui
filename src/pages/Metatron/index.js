6093b44a5f0152b3c13220b2c526512935acf789
John Erol Evangelista <je.evangelista04@gmail.com>
2019-02-14 16:47:28 -0500

    Fixed authentication bugs, added a temporary authentication method by doing an api query with the token as a header.

--- src/pages/Metatron/index.js
+++ src/pages/Metatron/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect } from 'react-router';
 import { Admin,
          Datagrid,
          fetchUtils,
@@ -14,6 +15,9 @@ import { Admin,
 import { base_url, fetch_meta } from '../../util/fetch/meta';
 import loopbackProvider from '../Admin/loopback-provider';
 import { PostFilter } from './signaturehelper';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 
 
 class Metatron extends React.PureComponent {
@@ -21,16 +25,14 @@ class Metatron extends React.PureComponent {
     super(props)
 
     this.state = {
-      LibraryList: null,
-      EntityList: null,
-      SignatureList: null,
-      library_stats: null,
-      entity_stats: null,
-      signature_stats: null,
+      library_stats: [],
+      entity_stats: [],
+      signature_stats: [],
       LibNum: 140,
       status: null,
       controller: null,
       token: null,
+      uid: "308de661-d3e2-11e8-8fe6-787b8ad942f3",
     }
     this.filterHandler = this.filterHandler.bind(this);
     this.LibraryList = this.LibraryList.bind(this);
@@ -38,6 +40,8 @@ class Metatron extends React.PureComponent {
     this.SignatureList = this.SignatureList.bind(this);
     this.httpClient = this.httpClient.bind(this);
     this.authProvider = this.authProvider.bind(this);
+    this.Dashboard = this.Dashboard.bind(this);
+    this.NotFound = this.NotFound.bind(this);
     this.dataProvider = loopbackProvider(base_url, this.httpClient);
 
   }
@@ -84,16 +88,16 @@ class Metatron extends React.PureComponent {
   }
 
   SignatureList(props){
-    return(
-      <List
-        {...props}
-        filters={
+    const filterForm = this.state.token===null ? false: 
           <PostFilter
             libnum={this.state.LibNum}
             filterhandler={this.filterHandler}
           />
-        }
-        filterDefaultValues={{"library": "308de661-d3e2-11e8-8fe6-787b8ad942f3"}}
+    return(
+      <List
+        {...props}
+        filters={filterForm}
+        filterDefaultValues={{"library": this.state.uid}}
       > 
         <Datagrid>
           <TextField
@@ -131,6 +135,28 @@ class Metatron extends React.PureComponent {
         </Datagrid>
       </List>
     )
+  }
+
+  Dashboard(props) {
+    return(
+      <Card>
+        <CardHeader title="Welcome to the administration" />
+        <CardContent>Let's start exploring</CardContent>
+      </Card>
+    )
+  }
+
+  NotFound(props) {
+    if(this.state.token){
+      return(
+        <Card>
+          <CardHeader title="Oop! I don't know what you are looking for" />
+          <CardContent>Check your link, please.</CardContent>
+        </Card>
+      )
+    }else{
+      return <Redirect to='/' />
+    }
   }
 
   // get_signatures(signature_stats) {
@@ -183,6 +209,7 @@ class Metatron extends React.PureComponent {
         this.setState({
           // signature_stats: signature_stats,
           signature_stats: signature_stats[0]["Signature_keys"],
+          uid: uid
         });
       } catch(e) {
         if(e.code !== DOMException.ABORT_ERR) {
@@ -195,11 +222,8 @@ class Metatron extends React.PureComponent {
 
   componentDidMount() {
     (async () => {
-      const headers = {'Authorization': `Basic ${this.state.token}`}
-      const { response: library_stats } = await fetch_meta('/libraries/key_count',
-                                                            undefined,
-                                                            undefined,
-                                                            headers)
+      // const headers = {'Authorization': `Basic ${this.state.token}`}
+      const { response: library_stats } = await fetch_meta('/libraries/key_count')
       this.setState({
         LibNum: library_stats.$validator,
         library_stats: library_stats,
@@ -207,31 +231,22 @@ class Metatron extends React.PureComponent {
       // this.get_libraries(library_stats)
     })();
     (async () => {
-      const uid = "308de661-d3e2-11e8-8fe6-787b8ad942f3"
-      const headers = {'Authorization': `Basic ${this.state.token}`}
-      const { response: signature_stats} = await fetch_meta('/libraries?filter={"where":{"id":"'+uid+'"}}',
-                                                            undefined,
-                                                            undefined,
-                                                            headers)
+      // const headers = {'Authorization': `Basic ${this.state.token}`}
+      const { response: signature_stats} = await fetch_meta('/libraries?filter={"where":{"id":"'+this.state.uid+'"}}')
       // console.log(signature_stats)
       // if("error" in signature_stats){
       //   const err = new Error(signature_stats["error"]["name"]);
       //   return err
       // }
       this.setState({
-        // SignatureList: (props) => <SigList {...props} signature_stats={signature_stats} />,
         signature_stats: signature_stats[0]["Signature_keys"],
       })
       // this.get_signatures(signature_stats)
     })();
     (async () => {
-      const headers = {'Authorization': `Basic ${this.state.token}`}
-      const { response: entity_stats } = await fetch_meta('/entities/key_count',
-                                                            undefined,
-                                                            undefined,
-                                                            headers)
+      // const headers = {'Authorization': `Basic ${this.state.token}`}
+      const { response: entity_stats } = await fetch_meta('/entities/key_count')
       this.setState({
-        // SignatureList: (props) => <SigList {...props} signature_stats={signature_stats} />,
         entity_stats: entity_stats,
       })
       // this.get_entities(entity_stats)
@@ -251,47 +266,54 @@ class Metatron extends React.PureComponent {
 
   async authProvider(type, params) {
     if (type === AUTH_LOGIN) {
-      this.setState({ token: Buffer.from(`${params.username}:${params.password}`).toString('base64') })
-      return Promise.resolve();
+      const token = Buffer.from(`${params.username}:${params.password}`).toString('base64')
+      const headers = {'Authorization': `Basic ${token}`}
+      const { response: auth_res} = await fetch_meta('/libraries?filter={"where":{"id":"'+this.state.uid+'"}}',
+                                                     undefined, undefined, headers)
+      console.log(auth_res)
+      if (("error" in auth_res) && (auth_res.error.statusCode >= 400 && auth_res.error.statusCode < 500)){
+        return Promise.reject()
+      }else{
+        this.setState({ token: token })
+        return Promise.resolve();
+      }
     }else if (type === AUTH_LOGOUT) {
         this.setState({ token: null })
         return Promise.resolve();
     }else if (type === AUTH_ERROR) {
-        if (params === 'DOMException: "The operation was aborted. "'){
-          const status  = params.status;
-          this.setState({ token: null })
-          return Promise.reject()
-        }else
-          return Promise.resolve()
+      if (params === 'DOMException: "The operation was aborted. "'){
+        const status  = params.status;
+        this.setState({ token: null })
+        return Promise.reject()
+      }else
+        return Promise.resolve()
     }else if (type === AUTH_CHECK) {
-        return this.state.token ? Promise.resolve() : Promise.reject();
+      return this.state.token ? Promise.resolve() : Promise.reject();
     }
     return Promise.reject()
   }
 
   render() {
-    return (
-      <Admin dataProvider={this.dataProvider} authProvider={this.authProvider}>
-        {this.state.library_stats  === null ? <div /> : (
-          <Resource
-            name="libraries"
-            list={this.LibraryList}
-          />
-        )}
-        {this.state.signature_stats === null ? <div /> : (
-          <Resource
-            name="signatures"
-            list={this.SignatureList}
-          />
-        )}
-        {this.state.entity_stats === null ? <div /> : (
-          <Resource
-            name="entities"
-            list={this.EntityList}
-          />
-        )}
-      </Admin>
-    )
+      return (
+        <Admin dataProvider={this.dataProvider}
+               authProvider={this.authProvider}
+               dashboard={this.Dashboard}
+               catchAll={this.NotFound}
+        >
+            <Resource
+              name="libraries"
+              list={this.LibraryList}
+            />
+            <Resource
+              name="signatures"
+              list={this.SignatureList}
+            />
+            <Resource
+              name="entities"
+              list={this.EntityList}
+            />
+        </Admin>
+      )
   }
 }
 
