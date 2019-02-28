ad816a64a516375ea0ec7597f4bdb6b931616e56
Daniel J. B. Clarke <u8sand@gmail.com>
2019-02-28 18:21:28 -0500

    Added page loading progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,17 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 import Head from 'next/head'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import '../styles/index.scss'
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', url => NProgress.start())
+Router.events.on('hashChangeStart', url => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('hashChangeComplete', url => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default class extends App {
   static async getInitialProps({ Component, ctx }) {
