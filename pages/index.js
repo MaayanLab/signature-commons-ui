c06ce6530ce19e8db73e7c38b0e00efc9f684b7f
Daniel J. B. Clarke <u8sand@gmail.com>
2019-02-28 18:04:21 -0500

    Updated index.js to function as before

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import Admin from './pages/Admin';
-import ArbitraryQuery from './pages/ArbitraryQuery';
-import DBCK from './pages/DBCK';
-import EntityPage from "./pages/EntityPage";
-import Home from './pages/Home';
-import Stats from './pages/Stats';
-import TermQuery from './pages/TermQuery';
-import Tests from './pages/Tests';
-import Values from './pages/Values';
+import dynamic from 'next/dynamic'
 
-const App = () => (
-  <Router>
-    <div className="root">
-      <Route path="/" component={Home} />
-      <Route exact path="/admin" component={Admin} />
-      <Route exact path="/arbitrary-query" component={ArbitraryQuery} />
-      <Route exact path="/dbck" component={DBCK} />
-      <Route exact path="/entity-page" component={EntityPage} />
-      <Route exact path="/stats" component={Stats} />
-      <Route exact path="/term-query" component={TermQuery} />
-      <Route exact path="/values" component={Values} />
-      <Route exact path="/tests" component={Tests} />
-    </div>
-  </Router>
+const Router = dynamic(async () => (await import('react-router-dom')).HashRouter, { ssr: false })
+const Route = dynamic(async () => (await import('react-router-dom')).Route, { ssr: false })
+const Admin = dynamic(() => import('../components/Admin'), { ssr: false })
+const ArbitraryQuery = dynamic(() => import('../components/ArbitraryQuery'), { ssr: false })
+const DBCK = dynamic(() => import('../components/DBCK'), { ssr: false })
+const EntityPage = dynamic(() => import("../components/EntityPage"), { ssr: false })
+const Home = dynamic(() => import('../components/Home'), { ssr: false })
+const Stats = dynamic(() => import('../components/Stats'), { ssr: false })
+const TermQuery = dynamic(() => import('../components/TermQuery'), { ssr: false })
+const Tests = dynamic(() => import('../components/Tests'), { ssr: false })
+const Values = dynamic(() => import('../components/Values'), { ssr: false })
+
+const App = (props) => (
+  <div className="root">
+    <Router>
+      <div className="root">
+        <Route path="/" component={Home} />
+        <Route exact path="/admin" component={Admin} />
+        <Route exact path="/arbitrary-query" component={ArbitraryQuery} />
+        <Route exact path="/dbck" component={DBCK} />
+        <Route exact path="/entity-page" component={EntityPage} />
+        <Route exact path="/stats" component={Stats} />
+        <Route exact path="/term-query" component={TermQuery} />
+        <Route exact path="/values" component={Values} />
+        <Route exact path="/tests" component={Tests} />
+      </div>
+    </Router>
+  </div>
 )
 
 export default App;
