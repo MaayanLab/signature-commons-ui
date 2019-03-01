8adad2302b418adfd0f8cea1d38a6811e4845fc0
Daniel J. B. Clarke <u8sand@gmail.com>
2019-03-01 18:13:24 -0500

    Setup deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 const withSass = require('@zeit/next-sass')
+const nextBuildId = require('next-build-id')
 module.exports = withSass({
+  target: 'serverless',
+  assetPrefix: process.env['PREFIX'] || '/sigcom',
   exportPathMap: async (defaultPathMap) => {
-    return {...defaultPathMap,
-    }
-  }
+    return {...defaultPathMap}
+  },
+  generateBuildId: async () => {
+    const fromGit = await nextBuildId({ dir: __dirname })
+    return fromGit.id
+  },
 })
