a44a013e933af11294e22f4095cf8f6b59dd2220
Daniel J. B. Clarke <u8sand@gmail.com>
2019-03-14 12:08:59 -0400

    Improved metadata search react-router implementation

---
+++ components/MetadataSearch/SearchResults.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import NProgress from 'nprogress'
+import { fetch_meta_post } from "../../util/fetch/meta";
+import dynamic from 'next/dynamic';
+
+const Signatures = dynamic(() => import('../../components/MetadataSearch/Signatures'))
+
+function build_where(q) {
+  if (q.indexOf(':') !== -1) {
+    const [key, ...value] = q.split(':')
+    return {
+      ['meta.' + key]: {
+        ilike: '%' + value.join(':') + '%'
+      }
+    }
+  } else {
+    return {
+      meta: {
+        fullTextSearch: q
+      }
+    }
+  }
+}
+
+export default class SearchResults extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      controller: undefined
+    }
+    this.performSearch = this.performSearch.bind(this)
+  }
+
+  componentDidMount() {
+    this.performSearch()
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.search !== this.props.search)
+      this.performSearch()
+  }
+
+  async performSearch() {
+    if(this.state.controller !== undefined) {
+      this.state.controller.abort()
+    }
+    try {
+      const controller = new AbortController()
+      NProgress.start()
+      this.setState({
+        status: 'Searching...',
+        signatures: undefined,
+        controller,
+      })
+
+      const where = build_where(this.props.search)
+
+      const start = Date.now()
+      const {duration: duration_meta_1, contentRange, response: signatures} = await fetch_meta_post('/signatures/find', {
+        filter: {
+          where,
+          limit: 20,
+        },
+      }, controller.signal)
+
+      const library_ids = [...new Set(signatures.map((sig) => sig.library))]
+      const {duration: duration_meta_2, response: libraries} = await fetch_meta_post('/libraries/find', {
+        filter: {
+          where: {
+            id: {
+              inq: library_ids
+            }
+          },
+        },
+      }, controller.signal)
+
+      const duration_meta = duration_meta_1 + duration_meta_2
+
+      const library_dict = libraries.reduce((L, l) => ({...L, [l.id]: l}), {})
+
+      for(const signature of signatures)
+        signature.library = library_dict[signature.library]
+
+      this.setState({
+        signatures,
+        status: '',
+        duration: (Date.now() - start) / 1000,
+        duration_meta,
+        count: contentRange.count,
+      }, () => NProgress.done())
+    } catch(e) {
+      NProgress.done()
+      if(e.code !== DOMException.ABORT_ERR) {
+        this.setState({
+          status: e + '',
+        })
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="col s12">
+        <div className="col s12 center">
+          {this.state.signatures !== undefined && this.state.count !== undefined ? (
+            <span className="grey-text">
+              Found {this.state.count}
+              {this.props.total_count !== undefined ? ` matches out of ${this.props.total_count} ` : null}
+              signatures
+              {this.state.duration_meta !== undefined  ? ` in ${this.state.duration_meta.toPrecision(3)} seconds` : null}
+            </span>
+          ) : null}
+        </div>
+
+        <div className="col s12">
+          {this.state.signatures !== undefined ? (
+            <Signatures
+              search={this.props.search}
+              signatures={this.state.signatures}
+            />
+          ) : null}
+        </div>
+      </div>
+    )
+  }
+}
--- components/MetadataSearch/index.js
+++ components/MetadataSearch/index.js
@@ -1,114 +1,52 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import NProgress from 'nprogress'
-import { fetch_meta_post, fetch_meta } from "../../util/fetch/meta";
+import { fetch_meta } from "../../util/fetch/meta";
 
 const SearchBox = dynamic(() => import('../../components/MetadataSearch/SearchBox'))
-const Signatures = dynamic(() => import('../../components/MetadataSearch/Signatures'))
+const SearchResults = dynamic(() => import('../../components/MetadataSearch/SearchResults'))
 
-function build_where(q) {
-  if (q.indexOf(':') !== -1) {
-    const [key, ...value] = q.split(':')
-    return {
-      ['meta.' + key]: {
-        ilike: '%' + value.join(':') + '%'
-      }
-    }
-  } else {
-    return {
-      meta: {
-        fullTextSearch: q
-      }
-    }
-  }
+function getParam(search, param) {
+  const params = new URLSearchParams(search)
+  let val = params.get(param)
+  if (val == undefined || val === null || val == undefined)
+    val = ''
+  return val
 }
 
 export default class extends React.Component {
   constructor(props) {
     super(props)
 
-    const params = new URLSearchParams(props.location.search)
-    const search = params.get('q')
-
     this.state = {
-      search,
-      params,
+      search: '',
+      currentSearch: '',
       controller: undefined,
       total_count: undefined,
     }
 
     this.searchChange = this.searchChange.bind(this)
-    this.performSearch = this.performSearch.bind(this)
   }
 
   async componentDidMount() {
-    this.performSearch(this.state.params.get('q'))
 
     const { response } = await fetch_meta('/signatures/count', {})
+    const currentSearch = getParam(this.props.location.search, 'q')
     this.setState({
+      currentSearch,
+      search: currentSearch,
       total_count: response.count
     })
   }
 
-  searchChange(e) {
-    this.setState({ search: e.target.value })
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearch = getParam(prevProps.location.search, 'q')
+    const currentSearch = getParam(this.props.location.search, 'q')
+    if (prevSearch !== currentSearch)
+      this.setState({ search: currentSearch, currentSearch })
   }
 
-  async performSearch(search) {
-    if(this.state.controller !== undefined) {
-      this.state.controller.abort()
-    }
-    try {
-      const controller = new AbortController()
-      NProgress.start()
-      this.setState({
-        status: 'Searching...',
-        controller,
-      })
-
-      const where = build_where(search)
-
-      const start = Date.now()
-      const {duration: duration_meta_1, contentRange, response: signatures} = await fetch_meta_post('/signatures/find', {
-        filter: {
-          where,
-          limit: 20,
-        },
-      }, controller.signal)
-
-      const library_ids = [...new Set(signatures.map((sig) => sig.library))]
-      const {duration: duration_meta_2, response: libraries} = await fetch_meta_post('/libraries/find', {
-        filter: {
-          where: {
-            id: {
-              inq: library_ids
-            }
-          },
-        },
-      }, controller.signal)
-
-      const duration_meta = duration_meta_1 + duration_meta_2
-
-      const library_dict = libraries.reduce((L, l) => ({...L, [l.id]: l}), {})
-
-      for(const signature of signatures)
-        signature.library = library_dict[signature.library]
-
-      this.setState({
-        signatures,
-        status: '',
-        duration: (Date.now() - start) / 1000,
-        duration_meta,
-        count: contentRange.count,
-      }, () => NProgress.done())
-    } catch(e) {
-      NProgress.done()
-      if(e.code !== DOMException.ABORT_ERR) {
-        this.setState({
-          status: e + '',
-        })
-      }
-    }
+  searchChange(e) {
+    this.setState({ search: e.target.value })
   }
 
   render() {
@@ -120,26 +58,12 @@ export default class extends React.Component {
             searchChange={this.searchChange}
           />
         </div>
-
-        <div className="col s12 center">
-          {this.state.signatures !== undefined && this.state.count !== undefined ? (
-            <span className="grey-text">
-              Found {this.state.count}
-              {this.state.total_count !== undefined ? ` matches out of ${this.state.total_count} ` : null}
-              signatures
-              {this.state.duration_meta !== undefined  ? ` in ${this.state.duration_meta.toPrecision(3)} seconds` : null}
-            </span>
-          ) : null}
-        </div>
-
-        <div className="col s12">
-          {this.state.signatures !== undefined ? (
-            <Signatures
-              search={this.state.params.get('q')}
-              signatures={this.state.signatures}
-            />
-          ) : null}
-        </div>
+        {this.state.currentSearch === '' ? null : (
+          <SearchResults
+            total_count={this.state.total_count}
+            search={this.state.currentSearch}
+          />
+        )}
       </div>
     )
   }
