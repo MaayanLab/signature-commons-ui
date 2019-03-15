fa8577c68c6a53cf5f2955511d48316166b9f9f5
Daniel J. B. Clarke <u8sand@gmail.com>
2019-03-15 12:34:09 -0400

    Broke up entity resolution and query
    - Now have realistic signature hash based on resolved entity uuids

--- components/SignatureSearch/index.js
+++ components/SignatureSearch/index.js
@@ -3,10 +3,12 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import GenesetSearchBox from "./GenesetSearchBox";
 import uuid5 from 'uuid5'
 import NProgress from 'nprogress'
-import { query } from "./query";
+import { query_overlap, query_rank } from "./query";
 import { get_library_resources } from "../Resources/resources";
 import ResourceFilters from "./ResourceFilters";
 import LibraryResults from "./LibraryResults";
+import { resolve_entities } from "./resolve";
+import { Set } from 'immutable'
 
 export default class SignatureSearch extends React.Component {
   constructor(props) {
@@ -28,8 +30,6 @@ export default class SignatureSearch extends React.Component {
     NProgress.start()
     // TODO: register signature with metadata api
 
-    const signature_id = uuid5(JSON.stringify(input))
-    console.log(input)
 
     let controller = this.state.controller
     if (controller !== null) controller.abort()
@@ -37,9 +37,55 @@ export default class SignatureSearch extends React.Component {
     this.setState(() => ({
       controller, input
     }), async () => {
-      const results = await query(this.state)
-      this.setState(() => ({ ...results }), () => NProgress.done())
-      this.props.history.push(`/SignatureSearch/${input.type}/${signature_id}`)
+      if (input.type === 'Overlap') {
+        const unresolved_entities = Set(input.geneset.toUpperCase().split(/[ \t\n;]+/).map(
+            // TODO: handle weights
+          (line) => /^(.+?)(,(.+))?$/.exec(line)[1]
+        ))
+        const { matched: entities, mismatched } = await resolve_entities({ entities: unresolved_entities, controller })
+        if (mismatched.count() > 0)
+          console.warn('mismatched entities', [...mismatched])
+
+        const resolved_entities = [...unresolved_entities].map((entity) => entities[entity])
+        const signature_id = uuid5(JSON.stringify(resolved_entities))
+
+        const results = await query_overlap({
+          ...this.state,
+          input: {
+            entities: resolved_entities,
+          }
+        })
+        this.setState(() => ({ ...results }), () => NProgress.done())
+        this.props.history.push(`/SignatureSearch/${input.type}/${signature_id}`)
+      } else if (input.type === 'Rank') {
+        const unresolved_up_entities = Set(input.up_geneset.toUpperCase().split(/[ \t\n;]+/).map(
+            // TODO: handle weights
+          (line) => /^(.+?)(,(.+))?$/.exec(line)[1]
+        ))
+        const unresolved_down_entities = Set(input.down_geneset.toUpperCase().split(/[ \t\n;]+/).map(
+            // TODO: handle weights
+          (line) => /^(.+?)(,(.+))?$/.exec(line)[1]
+        ))
+        const unresolved_entities = unresolved_up_entities.union(unresolved_down_entities)
+        const { matched: entities, mismatched } = await resolve_entities({ entities: unresolved_entities, controller })
+        if (mismatched.count() > 0)
+          console.warn('mismatched entities', [...mismatched])
+
+        const resolved_up_entities = [...unresolved_up_entities].map((entity) => entities[entity])
+        const resolved_down_entities = [...unresolved_down_entities].map((entity) => entities[entity])
+        const signature_id = uuid5(JSON.stringify([resolved_up_entities, resolved_down_entities]))
+  
+        const results = await query_rank({
+          ...this.state,
+          input: {
+            up_entities: resolved_up_entities,
+            down_entities: resolved_down_entities,
+          }
+        })
+        this.setState(() => ({ ...results }), () => NProgress.done())
+        this.props.history.push(`/SignatureSearch/${input.type}/${signature_id}`)
+      }
+
     })
   }
 
--- components/SignatureSearch/query.js
+++ components/SignatureSearch/query.js
@@ -3,65 +3,22 @@ import { fetch_data } from '../../util/fetch/data'
 import { Set } from 'immutable'
 import { maybe_fix_obj } from '../../util/maybe_fix_obj'
 
-export async function query(props) {
-  if (props.input.type === 'Overlap') {
-    return await query_overlap(props)
-  } else if (props.input.type === 'Rank') {
-    return await query_rank(props)
-  }
-}
-
 export async function query_overlap(props) {
-  let entities
-  entities = Set(
-    props.input.geneset.toUpperCase().split(/[ \t\n;]+/).map(
-       // TODO: handle weights
-      (line) => /^(.+?)(,(.+))?$/.exec(line)[1]
-    )
-  )
-  let entity_ids = Set()
-
   const start = Date.now()
-
-  const {duration: duration_meta_1, response: entity_meta_pre} = await fetch_meta_post('/entities/find', {
-    filter: {
-      where: {
-        'meta.Name': {
-          inq: entities.toArray(),
-        }
-      },
-      fields: [
-        'id',
-        'meta.Name',
-      ]
-    }
-  }, props.controller.signal)
-  const entity_meta = maybe_fix_obj(entity_meta_pre)
-
-  for(const entity of Object.values(entity_meta)) {
-    const matched_entities = entities.intersect(
-      Set([entity.meta.Name])
-    )
-
-    if (matched_entities.count() > 0) {
-      entities = entities.subtract(matched_entities)
-      entity_ids = entity_ids.add(entity.id)
-    }
-  }
-
-  console.log('matched:', entity_ids.count())
-  console.log('mismatched:', entities.count())
-
+  
   let duration_data = 0
   let count_data = 0
   let enriched_results
+
+  const entity_meta = maybe_fix_obj(props.input.entities)
+  const entities = props.input.entities.map((entity) => entity.id)
 
   const { response } = await fetch_data('/listdata')
 
   enriched_results = (await Promise.all(
     response.repositories.filter((repo) => repo.datatype === 'geneset_library').map((repo) =>
       fetch_data('/enrich/overlap', {
-        entities: entity_ids,
+        entities: entities,
         signatures: [],
         database: repo.uuid,
         limit: 500,
@@ -78,7 +35,7 @@ export async function query_overlap(props) {
     }, {}
   )
 
-  const {duration: duration_meta_2, response: enriched_signatures_meta} = await fetch_meta_post('/signatures/find', {
+  const {duration: duration_meta, response: enriched_signatures_meta} = await fetch_meta_post('/signatures/find', {
     filter: {
       where: {
         id: {
@@ -145,70 +102,32 @@ export async function query_overlap(props) {
     count_data,
     duration_data: duration_data,
     duration: (Date.now() - start)/1000,
-    duration_meta: duration_meta_1 + duration_meta_2,
-    matched_entities: entity_ids,
-    mismatched_entities: entities,
+    duration_meta: duration_meta,
     controller: null,
   }
 }
 
 export async function query_rank(props) {
-  let up_entities, down_entities, entities
-  up_entities = Set(props.input.up_geneset.toUpperCase().split(/[ \t\n;]+/).map((line) => /^(.+?)(,(.+))?$/.exec(line)[1])) // TODO: handle weights
-  down_entities = Set(props.input.down_geneset.toUpperCase().split(/[ \t\n;]+/).map((line) => /^(.+?)(,(.+))?$/.exec(line)[1])) // TODO: handle weights
-  entities = Set([...up_entities, ...down_entities])
-  let up_entity_ids = Set()
-  let down_entity_ids = Set()
-
   const start = Date.now()
-
-  const {duration: duration_meta_1, response: entity_meta_pre} = await fetch_meta_post('/entities/find', {
-    filter: {
-      where: {
-        'meta.Name': {
-          inq: entities.toArray(),
-        }
-      },
-      fields: [
-        'id',
-        'meta.Name',
-      ]
-    }
-  }, props.controller.signal)
-  const entity_meta = maybe_fix_obj(entity_meta_pre)
-
-  for(const entity of Object.values(entity_meta)) {
-    const matched_up_entities = up_entities.intersect(
-      Set([entity.meta.Name])
-    )
-    if (matched_up_entities.count() > 0) {
-      up_entities = up_entities.subtract(matched_up_entities)
-      up_entity_ids = up_entity_ids.add(entity.id)
-    }
-
-    const matched_down_entities = down_entities.intersect(
-      Set([entity.meta.Name])
-    )
-    if (matched_down_entities.count() > 0) {
-      down_entities = down_entities.subtract(matched_down_entities)
-      down_entity_ids = down_entity_ids.add(entity.id)
-    }
-  }
-
-  console.log('matched:', up_entity_ids.count() + down_entity_ids.count())
-  console.log('mismatched:', up_entities.count() + down_entities.count())
 
   let duration_data = 0
   let count_data = 0
   let enriched_results
 
+  const entity_meta = {
+    ...maybe_fix_obj(props.input.up_entities),
+    ...maybe_fix_obj(props.input.down_entities),
+  }
+  const up_entities = props.input.up_entities.map((entity) => entity.id)
+  const down_entities = props.input.down_entities.map((entity) => entity.id)
+  
   const { response } = await fetch_data('/listdata')
 
   enriched_results = (await Promise.all(
     response.repositories.filter((repo) => repo.datatype === 'rank_matrix').map((repo) =>
       fetch_data('/enrich/ranktwosided', {
-        up_entities: up_entity_ids,
-        down_entities: down_entity_ids,
+        up_entities: up_entities,
+        down_entities: down_entities,
         signatures: [],
         database: repo.uuid,
         limit: 500,
@@ -239,7 +158,7 @@ export async function query_rank(props) {
     }, {}
   )
 
-  const {duration: duration_meta_2, response: enriched_signatures_meta} = await fetch_meta_post('/signatures/find', {
+  const {duration: duration_meta, response: enriched_signatures_meta} = await fetch_meta_post('/signatures/find', {
     filter: {
       where: {
         id: {
@@ -306,9 +225,7 @@ export async function query_rank(props) {
     count_data,
     duration_data: duration_data,
     duration: (Date.now() - start)/1000,
-    duration_meta: duration_meta_1 + duration_meta_2,
-    matched_entities: up_entity_ids.union(down_entity_ids),
-    mismatched_entities: entities,
+    duration_meta: duration_meta,
     controller: null,
   }
 }
