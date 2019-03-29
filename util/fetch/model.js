944ae674c6f809dc50dd33349995a64659be69a6
Daniel J. B. Clarke <u8sand@gmail.com>
2019-03-29 13:52:30 -0400

    Added support for bulk data requests for efficient library data downloading
    References #70

--- util/fetch/model.js
+++ util/fetch/model.js
@@ -35,12 +35,29 @@ export default class DataProvider {
     serialized.id = await resource.id
     serialized.meta =  await resource.meta
 
+    if (!opts.fetched_data) {
+      if (opts.signatures === true)
+        await this.fetch_signatures_for_libraries(await resource.libraries)
+
+      if (opts.data === true) {
+        const signatures = (await PromiseAllSeq((await resource.libraries).map(
+          (library) =>
+            async () => await library.signatures
+        ))).reduce(
+          (all, sigs) => [...all, ...sigs], []
+        )
+        await this.fetch_data_for_signatures(signatures)
+      }
+    }
+
     if (opts.libraries === true) {
       serialized.libraries = await PromiseAllSeq(
         (await resource.libraries).map(
           (library) =>
             async () => await this.serialize_library(library, {
-              signatures: opts.signatures, data: opts.data
+              signatures: opts.signatures,
+              data: opts.data,
+              fetched_data: true,
             })
         )
       )
@@ -60,6 +77,14 @@ export default class DataProvider {
     serialized.dataset = await library.dataset
     serialized.dataset_type = await library.dataset_type
 
+    if (!opts.fetched_data) {
+      if (opts.signatures === true)
+        await this.fetch_signatures_for_libraries([library])
+
+      if (opts.data === true)
+        await this.fetch_data_for_signatures(await library.signatures)
+    }
+
     if (opts.resource === true)
       serialized.resource = await this.serialize_resource(await library.resource)
 
@@ -68,7 +93,7 @@ export default class DataProvider {
         (await library.signatures).map(
           (signature) =>
             async () => await this.serialize_signature(signature, {
-              data: opts.data
+              data: opts.data, fetched_data: true
             })
         )
       )
@@ -119,7 +144,7 @@ export default class DataProvider {
         library: opts.library, resource: opts.resource
       })
     }
-    
+
     return serialized
   }
 
@@ -251,16 +276,23 @@ export default class DataProvider {
     await this.fetch_resources()
   }
 
-  fetch_signatures_for_libraries = async () => {
+  fetch_signatures_for_libraries = async (libraries) => {
     // TODO: consider just fetching uuids here so we don't fetch the whole signature if
     //       we already have it
+    if (libraries === undefined) libraries = Object.values(this.libraries)
+
     const { response } = await fetch_meta_post({
       endpoint: `/signatures/find`,
       body: {
         filter: {
           where: {
             library: {
-              inq: Object.keys(this.libraries)
+              inq: await PromiseAllSeq(
+                libraries.map(
+                  (library) =>
+                    async () => await library.id
+                )
+              )
             }
           }
         }
@@ -271,7 +303,7 @@ export default class DataProvider {
       signature._fetched = true
       signature._signature = sig
 
-      const library = await this.resolve_library(sig.library)
+      const library = await signature.library
       if (library._signatures === undefined)
         library._signatures = new Set()
       library._signatures.add(signature)
@@ -299,10 +331,59 @@ export default class DataProvider {
       signature._fetched = true
       signature._signature = sig
 
-      const library = await this.resolve_library(sig.library)
+      const library = await signature.library
       if (library._signatures === undefined)
         library._signatures = new Set()
       library._signatures.add(signature)
+    }
+  }
+
+  fetch_data_for_signatures = async (signatures) => {
+    if (signatures === undefined) signatures = Object.values(this.signatures)
+    // filter by signatures not yet fetched
+    signatures = (await this.resolve_signatures(signatures)).filter((signature) => !signature._fetched_data)
+
+    // group signatures by (dataset, dataset_type)
+    const dataset_with_type_signatures = {}
+    for (const signature of signatures) {
+      const library = await signature.library
+      const dataset_with_type = JSON.stringify([await library.dataset, await library.dataset_type])
+      if (dataset_with_type_signatures[dataset_with_type] === undefined)
+        dataset_with_type_signatures[dataset_with_type] = new Set()
+      dataset_with_type_signatures[dataset_with_type].add(signature)
+    }
+    // go through groups and grab signatures
+    for (const dataset_with_type of Object.keys(dataset_with_type_signatures)) {
+      const [dataset, dataset_type] = JSON.parse(dataset_with_type)
+      const cur_signatures = [...dataset_with_type_signatures[dataset_with_type]]
+
+      // get the relevant endpoint to query
+      let endpoint
+      if (dataset_type.startsWith('geneset'))
+        endpoint = 'set'
+      else if (dataset_type.startsWith('rank'))
+        endpoint = 'rank'
+      else
+        throw `${dataset_type} not recognized`
+
+      // construct request with signatures of interest
+      const { response } = await fetch_data({
+        endpoint: `/fetch/${endpoint}`,
+        body: {
+          entities: [],
+          signatures: await PromiseAllSeq(cur_signatures.map((signature) => async () => await signature.id)),
+          database: dataset,
+        }
+      })
+
+      // resolve results
+      // TODO: Deal with rank data differently?
+      for (const sig of response.signatures) {
+        const signature = await this.resolve_signature(sig.uid)
+        signature._fetched_data = true
+        const entities = await this.resolve_entities(sig.entities)
+        signature._data = entities
+      }
     }
   }
 
@@ -352,10 +433,10 @@ export class Resource {
   get libraries() {
     return (async () => {
       if (this._libraries !== undefined)
-        return this._libraries
+        return [...this._libraries]
 
       await this._parent.fetch_libraries()
-      return this._libraries
+      return [...this._libraries]
     })()
   }
 
@@ -433,11 +514,11 @@ export class Library {
 
   get signatures() {
     return (async () => {
-      if (this._signatures !== undefined)
-        return this._signatures
-      
-      await this._parent.fetch_signatures_for_libraries()
-      return this._signatures
+      if (this._signatures !== undefined && this._signatures.length > 0)
+        return [...this._signatures]
+
+      await this._parent.fetch_signatures_for_libraries([this])
+      return [...this._signatures]
     })()
   }
 }
@@ -487,34 +568,8 @@ export class Signature {
     return (async () => {
       if (this._data !== undefined)
         return this._data
-      // TODO: deal with rank data
-      // TODO: deal with bulk data
 
-      const library = await this.library
-      const dataset_type = await library.dataset_type
-      let endpoint
-      if (dataset_type.startsWith('geneset'))
-        endpoint = 'set'
-      else if (dataset_type.startsWith('rank'))
-        endpoint = 'rank'
-      else
-        throw `${dataset_type} not recognized`
-
-      const { response } = await fetch_data({
-        endpoint: `/fetch/${endpoint}`,
-        body: {
-          entities: [],
-          signatures: [await this.id],
-          database: await library.dataset,
-        }
-      })
-      const entities = await PromiseAllSeq(
-        response.signatures[0].entities.map(
-          (entity) =>
-            async () => await this._parent.resolve_entity(entity)
-        )
-      )
-      this._data = entities
+      await this._parent.fetch_data_for_signatures([this])
       return this._data
     })()
   }
