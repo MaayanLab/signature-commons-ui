f0c8dd1fd47d2a17f300deb1de37e269e3fcddbf
John Erol Evangelista <je.evangelista04@gmail.com>
2019-06-18 17:01:38 -0400

    Added library and entity metadata search

--- components/MetadataSearch/SearchResults.js
+++ components/MetadataSearch/SearchResults.js
@@ -2,8 +2,10 @@ import React from 'react'
 import NProgress from 'nprogress'
 import { fetch_meta_post } from '../../util/fetch/meta'
 import dynamic from 'next/dynamic'
+import Typography from '@material-ui/core/Typography'
 
-const Signatures = dynamic(() => import('../../components/MetadataSearch/Signatures'))
+
+const MetaItem = dynamic(() => import('../../components/MetadataSearch/MetaItem'))
 
 function build_where(q) {
   if (q.indexOf(':') !== -1) {
@@ -33,15 +35,17 @@ export default class SearchResults extends React.Component {
   }
 
   componentDidMount() {
-    this.performSearch()
+    this.performSearch("signatures")
+    this.performSearch("libraries")
+    this.performSearch("entities")
   }
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.search !== this.props.search) {
-      this.performSearch()
+      this.performSearch("signatures")
     }
   }
 
-  async performSearch() {
+  async performSearch(table) {
     if (this.state.controller !== undefined) {
       this.state.controller.abort()
     }
@@ -57,46 +61,53 @@ export default class SearchResults extends React.Component {
       const where = build_where(this.props.search)
 
       const start = Date.now()
-      const { duration: duration_meta_1, contentRange, response: signatures } = await fetch_meta_post({
-        endpoint: '/signatures/find',
+      const { duration: duration_meta_1, contentRange, response: results } = await fetch_meta_post({
+        endpoint: `/${table}/find`,
         body: {
           filter: {
             where,
-            limit: 20,
+            limit: 5,
           },
         },
         signal: controller.signal,
       })
-
-      const library_ids = [...new Set(signatures.map((sig) => sig.library))]
-      const { duration: duration_meta_2, response: libraries } = await fetch_meta_post({
-        endpoint: '/libraries/find',
-        body: {
-          filter: {
-            where: {
-              id: {
-                inq: library_ids,
+      let duration_meta = duration_meta_1
+      if (table==="signatures"){
+        const library_ids = [...new Set(results.map((sig) => sig.library))]
+        const { duration: duration_meta_2, response: libraries } = await fetch_meta_post({
+          endpoint: '/libraries/find',
+          body: {
+            filter: {
+              where: {
+                id: {
+                  inq: library_ids,
+                },
               },
             },
           },
-        },
-        signal: controller.signal,
-      })
+          signal: controller.signal,
+        })
+        duration_meta = duration_meta_1 + duration_meta_2
+        const library_dict = libraries.reduce((L, l) => ({ ...L, [l.id]: l }), {})
+        for (const r of results) {
+          const lib_meta = {"id": library_dict[r.library].id,
+                       "meta": {
+                          "Library_name": library_dict[r.library].meta.Library_name 
+                        }
+                      }
+          r.library = lib_meta
+        }
 
-      const duration_meta = duration_meta_1 + duration_meta_2
-
-      const library_dict = libraries.reduce((L, l) => ({ ...L, [l.id]: l }), {})
-
-      for (const signature of signatures) {
-        signature.library = library_dict[signature.library]
       }
-
+      const duration_label = table + "_duration"
+      const duration_meta_label = table + "_duration_meta"
+      const count_label = table + "_count"
       this.setState({
-        signatures,
+        [table]: results,
         status: '',
-        duration: (Date.now() - start) / 1000,
-        duration_meta,
-        count: contentRange.count,
+        [duration_label]: (Date.now() - start) / 1000,
+        [duration_meta_label]: duration_meta,
+        [count_label]: contentRange.count,
       }, () => NProgress.done())
     } catch (e) {
       NProgress.done()
@@ -112,21 +123,79 @@ export default class SearchResults extends React.Component {
     return (
       <div className="col s12">
         <div className="col s12 center">
-          {this.state.signatures !== undefined && this.state.count !== undefined ? (
-            <span className="grey-text">
-              Found {this.state.count}
-              {this.props.total_count !== undefined ? ` matches out of ${this.props.total_count} ` : null}
-              signatures
-              {this.state.duration_meta !== undefined ? ` in ${this.state.duration_meta.toPrecision(3)} seconds` : null}
-            </span>
+          {this.state.signatures !== undefined && this.state.signatures_count !== undefined ? (
+            <div>
+              <Typography variant="title">
+               { "Signatures" }
+              </Typography>
+              <span className="grey-text">
+                Found {this.state.signatures_count} 
+                {this.props.signatures_total_count !== undefined ? ` matches out of ${this.props.signatures_total_count} ` : null}
+                signatures
+                {this.state.signatures_duration_meta !== undefined ? ` in ${this.state.signatures_duration_meta.toPrecision(3)} seconds` : null}
+              </span>
+            </div>
           ) : null}
         </div>
 
         <div className="col s12">
           {this.state.signatures !== undefined ? (
-            <Signatures
+            <MetaItem
               search={this.props.search}
-              signatures={this.state.signatures}
+              items={this.state.signatures}
+              type={"Signature"}
+            />
+          ) : null}
+        </div>
+
+        <div className="col s12 center">
+          {this.state.libraries !== undefined && this.state.libraries_count !== undefined ? (
+            <div>
+              <Typography variant="title">
+               { "Libraries" }
+              </Typography>
+              <span className="grey-text">
+                Found {this.state.libraries_count} 
+                {this.props.libraries_total_count !== undefined ? ` matches out of ${this.props.libraries_total_count} ` : null}
+                libraries
+                {this.state.libraries_duration_meta !== undefined ? ` in ${this.state.libraries_duration_meta.toPrecision(3)} seconds` : null}
+              </span>
+            </div>
+          ) : null}
+        </div>
+
+        <div className="col s12">
+          {this.state.libraries !== undefined ? (
+            <MetaItem
+              search={this.props.search}
+              items={this.state.libraries}
+              type={"Library"}
+            />
+          ) : null}
+        </div>
+
+        <div className="col s12 center">
+          {this.state.entities !== undefined && this.state.entities_count !== undefined ? (
+            <div>
+              <Typography variant="title">
+               { "Entities" }
+              </Typography>
+              <span className="grey-text">
+                Found {this.state.entities_count} 
+                {this.props.entities_total_count !== undefined ? ` matches out of ${this.props.entities_total_count} ` : null}
+                entities
+                {this.state.entities_duration_meta !== undefined ? ` in ${this.state.entities_duration_meta.toPrecision(3)} seconds` : null}
+              </span>
+            </div>
+          ) : null}
+        </div>
+
+        <div className="col s12">
+          {this.state.entities !== undefined ? (
+            <MetaItem
+              search={this.props.search}
+              items={this.state.entities}
+              type={"Entity"}
             />
           ) : null}
         </div>
