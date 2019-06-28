b20388a4d1a2461b8dd8b776527a3990d9bb7fec
John Erol Evangelista <je.evangelista04@gmail.com>
2019-06-28 13:40:43 -0400

    Fixed url parsing

--- components/ShowMeta.js
+++ components/ShowMeta.js
@@ -5,15 +5,13 @@ import { withStyles } from '@material-ui/core/styles'
 
 import { landingStyle } from '../styles/jss/theme.js'
 
-// https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url/5717133#5717133
 function validURL(str) {
-  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  return !!pattern.test(str);
+  try{
+    const url = new URL(str)
+  }catch{
+    return false
+  }
+  return true;
 }
 
 export function ShowMeta({ value, highlight, classes }) {
