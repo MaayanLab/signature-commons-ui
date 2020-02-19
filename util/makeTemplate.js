bfab36ad2013c7c2e4ca6a4e1eda0b02ac162600
John Erol Evangelista <je.evangelista04@gmail.com>
2020-02-19 10:38:05 -0500

    Added mechanism for object resolution

--- util/makeTemplate.js
+++ util/makeTemplate.js
@@ -1,19 +1,45 @@
 export function makeTemplate(
     templateString,
-    templateVariables,
-    templateSubfield = null
+    templateVariables
 ) {
   const keys = [...Object.keys(templateVariables).map((key) => key.replace(/ /g, '_')), 'PREFIX']
   const values = [...Object.values(templateVariables), process.env.PREFIX]
   let templateFunction = new Function(...keys, `return \`${templateString}\`;`)
-  if (templateSubfield) {
-    if (templateFunction(...values) !== 'undefined') {
-      templateFunction = new Function(...keys, `return \`${templateSubfield}\`;`)
-    }
-  }
   try {
     return templateFunction(...values)
   } catch (error) {
+    return 'undefined'
+  }
+}
+
+export function makeTemplateForObject(
+  templateString,
+  templateVariables,
+  templateSubfield = null
+) {
+  const extracted_str = makeTemplate(templateString, templateVariables)
+  if (extracted_str === 'undefined') return extracted_str
+  try {
+    const extracted = JSON.parse(extracted_str)
+    let result = 'undefined'
+    if (typeof extracted === 'object'){
+      if (Array.isArray(extracted)) {
+        result = []
+        for (const e of extracted){
+          if (templateSubfield!==null && typeof e === 'object') {
+            result = [...result, makeTemplate(templateSubfield, e)]
+          } else if (typeof e === "string"){
+            result = [...result, e]
+          }
+        }
+        return result
+      } else {
+        return makeTemplate(templateSubfield, extracted)
+      }
+    }
+    return result
+  }catch (error) {
+    console.error(error)
     return 'undefined'
   }
 }
