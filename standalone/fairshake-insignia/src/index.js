c0ec5195a1c1c6580bb2c9e9733a70aa02996eae
John Erol Evangelista <je.evangelista04@gmail.com>
2020-02-25 14:20:18 -0500

    Fixed svg rendering

--- standalone/fairshake-insignia/src/index.js
+++ standalone/fairshake-insignia/src/index.js
@@ -2,30 +2,39 @@ import React from 'react'
 import { build_svg_from_score } from 'fairshakeinsignia'
 import PropTypes from 'prop-types'
 import CircularProgress from '@material-ui/core/CircularProgress'
-
+import * as d3 from 'd3'
 
 export default class Insignia extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             ref: null,
-            error: ''
+            error: '',
+            reset: false
         }
+        this.updating = false
     }
 
     ref_callback = ref => {
-        if (this.state.ref!==ref){
+        if (this.state.ref === null && this.state.ref!==ref){
             this.setState({
                 ref
             })
         }
     }
 
-    componentDidUpdate = (prevProps, prevState) => {
-        if (prevState.ref !== this.state.ref) {
-            const { params } = this.props
-            build_svg_from_score(this.state.ref, params)
-        }
+    svgContainer = () => (
+        <div ref={this.ref_callback} style={{width: 40, height:40, ...styles}} ></div>
+    )
+
+    componentDidUpdate = async (prevProps, prevState) => {
+        const { params } = this.props
+        if (this.state.ref === null || this.updating === true) return
+        this.updating = true
+        for (const child of this.state.ref.children)
+            this.state.ref.removeChild(child)
+        await build_svg_from_score(this.state.ref, params)
+        this.updating = false
     }
 
     render = () => {
