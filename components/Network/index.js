0bb7eb8eba5d62ad62dddcd105cb8265f3b12694
John Erol Evangelista <je.evangelista04@gmail.com>
2020-03-02 15:18:12 -0500

    Fixed link

--- components/Network/index.js
+++ components/Network/index.js
@@ -42,7 +42,7 @@ export default class Network extends React.PureComponent {
                 <Grid item xs={12} md={2}>
                     <Grid container direction={"column"} style={{marginLeft: 10}}>
                         <Grid item>
-                            <img src="https://amp.pharm.mssm.edu/mcf10a-deep-dive/app/static/R/networks/network_legend.png"
+                            <img src="https://amp.pharm.mssm.edu/mcf10a-notebooks/app/static/R/networks/network_legend.png"
                                 style={{height: 300}}
                             ></img>
                         </Grid>
