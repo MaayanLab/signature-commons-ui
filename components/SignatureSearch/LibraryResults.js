b9b27b33857acc5be0325be41817d5acb2304730
erol <je.evangelista04@gmail.com>
2020-05-21 11:20:11 -0400

    Moved sorted_results from render to componentdidupdate/mount

--- components/SignatureSearch/LibraryResults.js
+++ components/SignatureSearch/LibraryResults.js
@@ -4,7 +4,7 @@ import MUIDataTable from 'mui-datatables'
 import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
 import ShowMeta from '../../components/ShowMeta'
-import { Label } from '../../components/Label'
+import Grid from '@material-ui/core/Grid'
 import { findMatchedSchema } from '../../util/objectMatch'
 import { makeTemplate } from '../../util/makeTemplate'
 import { RunningSum, dataFromResults } from '@dcic/signature-commons-ui-components-running-sum'
@@ -12,6 +12,9 @@ import { fetch_data } from '../../util/fetch/data'
 import Lazy from '../Lazy'
 import { createMuiTheme } from '@material-ui/core'
 import { buildHead, buildBody } from './mui-datatables-utils'
+import { get_card_data } from '../MetadataSearch/MetadataSearchResults'
+import DataTable from '../MetadataSearch/DataTable'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const one_tailed_columns = [
   'P-Value',
@@ -68,6 +71,14 @@ export const rank_data_results = async ({ up, down, signature, database }) => {
 }
 
 export default class LibraryResults extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      sorted_results: null,
+    }
+  }
+  
   check_column = ({ schema, prop, item }) => {
     if (schema.properties[prop].text === undefined) {
       return false
@@ -233,131 +244,178 @@ export default class LibraryResults extends React.Component {
     )
   }
 
-  render() {
+  componentDidMount = () => {
     const sorted_results = [...this.props.results].sort((a, b) => b.signatures.length - a.signatures.length)
+                            .map(r=>{
+                              const data = get_card_data(r.library,this.props.schemas)
+                              return {
+                                ...data,
+                                signatures: r.signatures,
+                              }
+                            })
+    this.setState({
+      sorted_results
+    })
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.location.pathname !== this.props.location.pathname){
+      const sorted_results = [...this.props.results].sort((a, b) => b.signatures.length - a.signatures.length)
+                            .map(r=>{
+                              const data = get_card_data(r.library,this.props.schemas)
+                              return {
+                                ...data,
+                                signatures: r.signatures,
+                              }
+                            })
+      this.setState({
+        sorted_results
+      })
+    }
+  }
+
+  render() {
+    if (this.state.sorted_results === null) {
+      return <CircularProgress />
+    }
     return (
-      <div className="col s12">
-        <ul
-          className="collapsible popout"
-          ref={(ref) => M.Collapsible.init(ref, {
-            onOpenStart: () => window.dispatchEvent(new Event('resize')),
-            onOpenEnd: () => window.dispatchEvent(new Event('resize')),
-          })}
-        >
-          {sorted_results.map((result, ind) => (
-            <li
-              key={result.library.id}
-              className={ind === 0 ? 'active' : ''}
-            >
-              <div
-                className="page-header collapsible-header"
-                style={{
-                  padding: 10,
-                  display: 'flex',
-                  flexDirection: 'row',
-                  backgroundColor: 'rgba(255,255,255,1)',
-                }}
-              >
-                <style jsx>{`
-                  .counter {
-                    font-size: 75%;
-                    line-height: 2.2em;
-                    z-index: 100;
-                    color: white;
-                    border-radius: 50%;
-                    width: 25px;
-                    height: 25px;
-                    text-align: center;
-                    vertical-align: middle;
-                  }
-                `}</style>
-                <Label
-                  item={result.library}
-                  visibility={1}
-                  schemas={this.props.schemas}
-                />
-                <div style={{ flex: '1 0 auto' }}>&nbsp;</div>
-                <div className="counter red lighten-1">
-                  {result.signatures.length}
-                </div>
-                <div
-                  style={{ border: 0, cursor: 'pointer' }}
-                >
-                  <i className="material-icons">expand_more</i>
-                </div>
-              </div>
-              <div
-                className="collapsible-body"
-              >
-                <style jsx>{`
-                  .tab-content {
-                    height: 600px;
-                  }
-                `}</style>
-                <div
-                  style={{
-                    paddingTop: 0,
-                    marginTop: '-25px',
-                  }}
-                >
-                  {/*
-                  <ul
-                    className="tabs"
-                    ref={(ref) => M.Tabs.init(ref, {
-                      onShow: () => window.dispatchEvent(new Event('resize'))
-                    })}
-                  >
-                    <li className="tab col s3"><a className="active" href={"#bargraph-" + result.library.id }>Bar Graph</a></li>
-                    <li className="tab col s3"><a href={"#table-" + result.library.id }>Table</a></li>
-                  </ul>
-                  <div id={"bargraph-" + result.library.id } className="tab-content">
-                    {(() => {
-                      let signatures = [...result.signatures].sort(
-                        (a, b) => b.meta['p-value'] - a.meta['p-value']
-                      ).slice(0, 10).map((signature, ind) => ({
-                        y: ind,
-                        text: signature.meta['Original_String'],
-                        x: -Math.log10(signature.meta['p-value']),
-                      }))
-                      const data = [{
-                        name: '-log(p-value)',
-                        orientation: 'h',
-                        type: 'bar',
-                        textposition: 'auto',
-                        align: 'left',
-                        y: signatures.map((s) => s.y),
-                        x: signatures.map((s) => s.x),
-                        text: signatures.map((s) => s.text),
-                      }]
-                      return (
-                        <Plot
-                          layout={{
-                            barmode: 'stack',
-                            yaxis: {
-                              ticktext: signatures.map((s) => s.x.toPrecision(3)),
-                              tickvals: signatures.map((s) => s.y),
-                            },
-                          }}
-                          config={{
-                            displayModeBar: false
-                          }}
-                          useResizeHandler={true}
-                          style={{width: '100%', height: '100%'}}
-                          data={data}
-                        />
-                      )
-                    })()}
-                  </div>
-                  */}
-                  <div id={'table-' + result.library.id } className="tab-content">
-                    {this.render_table({ result })}
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <div>
+        <Grid container spacing={32} style={{marginBottom:10}}>
+          <Grid item xs={12}>
+            <DataTable schemas={this.props.schemas}
+              ui_values={this.props.ui_values}
+              collection={this.state.sorted_results}
+              loaded={true}
+              current_table={"libraries"}
+              history={this.props.history}
+              deactivate_download={true}
+              expandRenderer={({data, ...props}) => this.render_table({ result: data })}
+            />
+          </Grid>
+        </Grid>
       </div>
+      // <div className="col s12">
+      //   <ul
+      //     className="collapsible popout"
+      //     ref={(ref) => M.Collapsible.init(ref, {
+      //       onOpenStart: () => window.dispatchEvent(new Event('resize')),
+      //       onOpenEnd: () => window.dispatchEvent(new Event('resize')),
+      //     })}
+      //   >
+      //     {sorted_results.map((result, ind) => (
+      //       <li
+      //         key={result.original.id}
+      //         className={ind === 0 ? 'active' : ''}
+      //       >
+      //         <div
+      //           className="page-header collapsible-header"
+      //           style={{
+      //             padding: 10,
+      //             display: 'flex',
+      //             flexDirection: 'row',
+      //             backgroundColor: 'rgba(255,255,255,1)',
+      //           }}
+      //         >
+      //           <style jsx>{`
+      //             .counter {
+      //               font-size: 75%;
+      //               line-height: 2.2em;
+      //               z-index: 100;
+      //               color: white;
+      //               border-radius: 50%;
+      //               width: 25px;
+      //               height: 25px;
+      //               text-align: center;
+      //               vertical-align: middle;
+      //             }
+      //           `}</style>
+      //           <Label
+      //             item={result.original}
+      //             visibility={1}
+      //             schemas={this.props.schemas}
+      //           />
+      //           <div style={{ flex: '1 0 auto' }}>&nbsp;</div>
+      //           <div className="counter red lighten-1">
+      //             {result.signatures.length}
+      //           </div>
+      //           <div
+      //             style={{ border: 0, cursor: 'pointer' }}
+      //           >
+      //             <i className="material-icons">expand_more</i>
+      //           </div>
+      //         </div>
+      //         <div
+      //           className="collapsible-body"
+      //         >
+      //           <style jsx>{`
+      //             .tab-content {
+      //               height: 600px;
+      //             }
+      //           `}</style>
+      //           <div
+      //             style={{
+      //               paddingTop: 0,
+      //               marginTop: '-25px',
+      //             }}
+      //           >
+      //             {/*
+      //             <ul
+      //               className="tabs"
+      //               ref={(ref) => M.Tabs.init(ref, {
+      //                 onShow: () => window.dispatchEvent(new Event('resize'))
+      //               })}
+      //             >
+      //               <li className="tab col s3"><a className="active" href={"#bargraph-" + result.library.id }>Bar Graph</a></li>
+      //               <li className="tab col s3"><a href={"#table-" + result.library.id }>Table</a></li>
+      //             </ul>
+      //             <div id={"bargraph-" + result.library.id } className="tab-content">
+      //               {(() => {
+      //                 let signatures = [...result.signatures].sort(
+      //                   (a, b) => b.meta['p-value'] - a.meta['p-value']
+      //                 ).slice(0, 10).map((signature, ind) => ({
+      //                   y: ind,
+      //                   text: signature.meta['Original_String'],
+      //                   x: -Math.log10(signature.meta['p-value']),
+      //                 }))
+      //                 const data = [{
+      //                   name: '-log(p-value)',
+      //                   orientation: 'h',
+      //                   type: 'bar',
+      //                   textposition: 'auto',
+      //                   align: 'left',
+      //                   y: signatures.map((s) => s.y),
+      //                   x: signatures.map((s) => s.x),
+      //                   text: signatures.map((s) => s.text),
+      //                 }]
+      //                 return (
+      //                   <Plot
+      //                     layout={{
+      //                       barmode: 'stack',
+      //                       yaxis: {
+      //                         ticktext: signatures.map((s) => s.x.toPrecision(3)),
+      //                         tickvals: signatures.map((s) => s.y),
+      //                       },
+      //                     }}
+      //                     config={{
+      //                       displayModeBar: false
+      //                     }}
+      //                     useResizeHandler={true}
+      //                     style={{width: '100%', height: '100%'}}
+      //                     data={data}
+      //                   />
+      //                 )
+      //               })()}
+      //             </div>
+      //             */}
+      //             <div id={'table-' + result.original.id } className="tab-content">
+      //               {this.render_table({ result })}
+      //             </div>
+      //           </div>
+      //         </div>
+      //       </li>
+      //     ))}
+      //   </ul>
+      // </div>
     )
   }
 }
