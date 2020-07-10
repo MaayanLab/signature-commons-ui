d4f1d811d64b1bb2c83721a69f209f7f9950c531
erol <je.evangelista04@gmail.com>
2020-07-10 12:26:22 -0400

    Added color to status

--- components/MetadataSearch/UpChecker.js
+++ components/MetadataSearch/UpChecker.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Chip from '@material-ui/core/Chip'
 import Icon from '@material-ui/core/Icon'
 import { fetch_external } from "../../util/fetch/fetch_external";
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
 
 export default class UpChecker extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            up: "Loading..."
+            up: "Loading...",
+            style: {}
+
         }
 
     }
@@ -16,7 +20,10 @@ export default class UpChecker extends React.Component {
             endpoint: `/api?url=${this.props.url}`,
         })
         this.setState({
-            up: response.status
+            up: response.status,
+            style: {
+                background: response.status === "yes"? green[300]: red[200]
+            }
         })
     }
 
@@ -26,8 +33,9 @@ export default class UpChecker extends React.Component {
             <Chip
                 className={classes.chip}
                 key={"up"}
+                style={this.state.style}
                 avatar={<Icon className={`${classes.icon} mdi ${this.state.up==="yes"? "mdi-cloud": "mdi-cloud-alert"} mdi-18px`} />}
-                label={<span>Up: {this.state.up}</span>}
+                label={<span>Status: {this.state.up==="yes"?"Online": this.state.up}</span>}
             />
         )
     }
