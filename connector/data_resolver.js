266933977f7fb13016f466cf0a25ec3428fcf0f0
erol <je.evangelista04@gmail.com>
2020-11-04 18:56:00 -0500

    Added connector functions

---
+++ connector/data_resolver.js
@@ -0,0 +1,366 @@
+import { fetch_meta_post } from '../util/fetch/meta'
+import { fetch_data } from '../util/fetch/data'
+import { Model, Signature, Entity } from './model'
+import isUUID from 'validator/lib/isUUID'
+import { Set } from 'immutable'
+
+const entry_model = {
+	resources: Model,
+	libraries: Model,
+	signatures: Signature,
+	entities: Entity,
+}
+
+const fetch_endpoint = {
+    geneset_library: "/fetch/set",
+    rank_matrix: "/fetch/rank"
+}
+
+const enrich_endpoint = {
+    geneset_library: {
+        set: "/enrich/overlap",
+        up_down: "/enrich/overlap"
+    },
+    rank_matrix: {
+        up_down: "/enrich/ranktwosided",
+        set: "/enrich/rank"
+    }
+}
+
+export default class DataResolver {
+	constructor(){
+		this.data_repo = {
+			resources: {},
+			libraries: {},
+			signatures: {},
+			entities: {}
+		}
+		this._controller = null
+	}
+
+	controller = () => {
+		this._controller = new AbortController()
+		return this._controller
+	}
+
+	abort_controller = () => {
+		this._controller.abort()
+	}
+
+	resolve_entries = async ({model, entries, filter={}, parent}) => {
+		const start_time = new Date()
+		this.controller()
+		const resolved_entries = {}
+        const unresolved_entries = {}
+		const invalid_entries = []
+		for (const entry of entries) {
+			if (typeof entry === "string"){
+				if (isUUID(entry)){
+					const resolved_entry = this.data_repo[model][entry]
+					if (typeof resolved_entry !== "undefined"){
+						resolved_entries[entry] = resolved_entry
+					}else {
+						const entry_object = new entry_model[model](model, entry, this)
+						unresolved_entries[entry] = entry_object
+					}
+				}else {
+					invalid_entries.push(entry)
+				}
+			} else if (typeof entry === "object"){
+				if (typeof entry.id !== "undefined" && isUUID(entry.id)){
+					const resolved_entry = this.data_repo[model][entry.id]
+					if (typeof resolved_entry !== "undefined"){
+						resolved_entries[entry.id] = resolved_entry
+					} else {
+						const entry_object = new entry_model[model](model, entry, this)
+						unresolved_entries[entry.id] = entry_object
+					}
+				}else {
+					invalid_entries.push(entry)
+				}
+			} else if (entry instanceof Model){
+				const resolved_entry = this.data_repo[model][entry.id]
+				if (typeof resolved_entry !== "undefined"){
+					resolved_entries[entry.id] = resolved_entry
+				} else {
+					await entry.validate_entry()
+					if (!entry.resolved){
+						unresolved_entries[entry.id] = entry
+					}else {
+						resolved_entries[entry.id] = entry
+						this.data_repo[model][entry.id] = entry
+					}
+					
+				}
+			}
+		}
+		if (Object.keys(unresolved_entries).length > 0){
+			filter = {
+				...filter,
+				where: {
+					and: [
+						filter.where,
+						{
+							id: {
+								inq: Object.keys(unresolved_entries)
+							}
+						}
+					]
+				}
+			}
+			const { response: entries } = await fetch_meta_post({
+				endpoint: `/${model}/find`,
+				body: {
+				  filter,
+				},
+				signal: this._controller.signal,
+			  })
+			for (const e of entries){
+				let entry = this.data_repo[model][e.id]
+				if (entry === undefined){
+					// If you don't have a field option, then you are resolving everything
+					entry = new entry_model[model](model, e, this, filter.fields===undefined, parent)
+					resolved_entries[entry.id] = entry
+				} else {
+					entry.update_entry(e)
+				}
+				
+				this.data_repo[model][entry.id] = entry
+				resolved_entries[entry.id] = entry
+			}
+		}
+		return { 
+			resolved_entries: resolved_entries, 
+			invalid_entries: invalid_entries,
+			count: Object.keys(resolved_entries).length,
+			duration: (new Date() - start_time) / 1000
+		}
+	}
+
+	filter_metadata = async ({model, filter, parent}) => {
+		const start_time = new Date()
+		this.controller()
+		const resolved_entries = {}
+		const { response: entries, contentRange, duration } = await fetch_meta_post({
+			endpoint: `/${model}/find`,
+			body: {
+			  filter,
+			},
+			signal: this._controller.signal,
+		  })
+		for (const e of entries){
+			let entry = this.data_repo[model][e.id]
+			if (entry === undefined){
+				// If you don't have a field option, then you are resolving everything
+				entry = new entry_model[model](model, e, this, filter.fields===undefined, parent)
+				resolved_entries[entry.id] = entry
+			} else {
+				entry.update_entry(e)
+			}
+			
+			this.data_repo[model][entry.id] = entry
+			resolved_entries[entry.id] = entry
+		}
+		return {
+			entries: resolved_entries,
+			count: contentRange.count,
+			duration: (new Date() - start_time) / 1000
+		}
+	}
+
+	get_entities_from_signatures = async (entries) => {
+		this.controller()
+		const dataset_id_map = {}
+        for (const entry of entries){
+			await entry.entry()
+			const uid = entry.id
+			const parent = await (await entry.parent()).entry()
+            const dataset_type = parent["dataset_type"]
+			const dataset = parent["dataset"]
+            if (dataset_id_map[dataset] === undefined){
+				dataset_id_map[dataset] = {
+                    endpoint: fetch_endpoint[dataset_type],
+                    dataset_type: dataset_type,
+                    signatures: []
+                }
+			}
+            dataset_id_map[dataset]["signatures"].push(uid)
+		}
+		
+		let signature_entity_map = {}
+		let entities = Set()
+		for (const [dataset, val] of Object.entries(dataset_id_map)) {
+			const body = {
+                database: dataset,
+                signatures: val.signatures
+			}
+
+			const {response} = await fetch_data({
+				endpoint: val.endpoint,
+				body,
+				signal: this._controller.signal,
+			  })
+		
+			if (val.dataset_type === "geneset_library"){
+				for (const sig of response.signatures){
+					signature_entity_map[sig["uid"]] = sig["entities"]
+					entities = entities.union(sig["entities"])
+				}
+			}else {
+				entities = entities.union(response.entities)
+				for (const sig of response.signatures){
+					const ranks = sig.ranks
+					const ranked = response.entities.map((ent, ind) => ({ ent, rank: ranks[ind] }))
+					ranked.sort(({ rank: rank_a }, { rank: rank_b }) => rank_a - rank_b)
+					signature_entity_map[sig.uid] = ranked.reduce((agg, { ent }) => ent !== undefined ? [...agg, ent] : agg, [])
+				}
+			}
+			
+		}
+		return {
+			results: signature_entity_map,
+			all_entities: entities.toArray(),
+		}
+	}
+
+	enrich_entities = async ({entity_type, datatype, ...query}) => {
+		const start_time = new Date()
+		this.controller()
+		const body = {
+            "limit": 10,
+            ...query
+		}
+		const endpoint = enrich_endpoint[data_type][entity_type]
+		const {response, contentRange } = await fetch_data({
+			endpoint,
+			body,
+			signal: this._controller.signal,
+		  })
+		const signatures = response.results.map(({uuid, ...meta})=>({
+			id: uuid,
+			meta
+		}))
+
+		return {
+            entries: signatures,
+            count: contentRange.count,
+            duration: (new Date() - start_time) / 1000,
+            database: query.database
+        }
+	}
+
+	get_signatures_from_entities = async ({query, repo}) => {
+		const start_time = new Date()
+		let entity_type
+		if (query.entities !== undefined){
+			entity_type = "set"
+		} else if (query.up_entities !== undefined && query.down_entities !== undefined){
+			entity_type = "up_down"
+		} else {
+			throw new Error("Invalid query: " + query)
+		}
+
+		if (repo === undefined) {
+			const { response: r } = await fetch_data({ endpoint: '/listdata' })
+			repo = r
+		}
+		let all_count = 0
+		let signatures = []
+		const results = {}
+		for (const r of repo.repositories){
+			const {uuid: database, datatype} = r
+			if (entity_type === "set"){
+				const query = {
+                    ...query,
+                    database,
+                    entity_type,
+                    datatype
+				}
+				const {entries, count:c} = await this.enrich_entities(query)
+				all_count += c
+                signatures = [...signatures, ...entries]
+				results[database] = {"set": entries, query}
+				
+			}else if (entity_type === "up_down"){
+				if (datatype === "rank_matrix"){
+					const query = {
+                        ...query,
+                        database,
+                        entity_type,
+                        datatype,
+					}
+					const {entries, count:c} = await this.enrich_entities(query)
+					all_count += c
+					signatures = [...signatures, ...entries]
+					results[database] = {rank: entries, query}
+				} else {
+					// Up genes
+                    const up_query = {
+                        limit: query.limit,
+                        offset: query.offset,
+                        entities: query.up_entities,
+                        database: database,
+                        entity_type: entity_type,
+                        datatype: data_type
+                    }
+					const {entries: up_entries, count:up_count} = await this.enrich_entities(up_query)
+					all_count += up_count
+					signatures = [...signatures, ...up_entries]
+					results[database] = {up: up_entries, query}
+
+					// Down genes
+                    const down_query = {
+                        limit: query.limit,
+                        offset: query.offset,
+                        entities: query.down_entities,
+                        database: database,
+                        entity_type: entity_type,
+                        datatype: data_type
+                    }
+					const {entries: down_entries, count:down_count} = await this.enrich_entities(down_query)
+					all_count += down_count
+					signatures = [...signatures, ...down_entries]
+					results[database] = {down: down_entries, query}
+				}
+			}
+		}
+
+		const {resolved_entries} = await this._data_resolver.resolve_entries(
+			{
+				model: "signatures",
+				entries: signatures
+			}
+		)
+		const resolved_results = {}
+		for (const [k,v] of Object.entries(resolved_entries)){
+			resolved_results[k] = {}
+			for (const [key,val] of Object.entries(resolved_entries)){
+				resolved_results[k][key] = []
+				for (const e of val){
+					const entry = resolved_entries[e.id]
+					if (entry!==undefined){
+						const ent = entry.entry()
+						const resolved_entry_meta  = ent.meta || {}
+						const unresolved_entry_meta = e.entry().meta || {}
+						const updated_entry = {
+							...ent,
+							meta: {
+								...unresolved_entry_meta,
+								...resolved_entry_meta
+							}
+						}
+						entry.update_entry(updated_entry)
+						resolved_results[k][key].push(entry)
+						this.data_repo["signatures"][entry.id] = entry
+					}
+				}
+			}
+		}
+		return {
+			entries: resolved_results,
+            count: all_count,
+            duration: (new Date() - start_time) / 1000,
+		}
+	}
+
+}
---
+++ connector/model.js
@@ -0,0 +1,201 @@
+import { validate } from '@maayanlab/signature-commons-schema'
+import isUUID from 'validator/lib/isUUID'
+import merge from 'deepmerge'
+
+const singular_form = {
+    resources: "resource",
+    libraries: "library",
+    signatures: "signature",
+    entities: "entity"
+}
+
+const parent_mapper = {
+    resources: null,
+    libraries: "resources",
+    signatures: "libraries",
+    entities: null
+}
+
+const child_mapper = {
+    resources: "libraries",
+    libraries: "signatures",
+    signatures: "entities",
+    entities: "signatures"
+}
+
+export class Model {
+	constructor(model, entry, data_resolver=null, resolved=false, parent=undefined){
+		if (data_resolver===null){
+			throw new Error(`${model} should be instantiated in a data repository`)
+		}
+		this._data_resolver = data_resolver
+		this.resolved = resolved
+		this._parent = parent
+		this._children = undefined
+        this._children_count = undefined
+        this._model = model
+        this._parent_model = parent_mapper[model]
+		this._child_model = child_mapper[model]
+		if (typeof entry === "string" && isUUID(entry)){
+			this._entry = {"id": entry}
+            this.resolved = false 
+		}else if (!(entry instanceof Array) && typeof entry === "object"){
+			this._entry = entry
+			// if (!this.resolved){
+			// 	this.validate_entry()
+			// }
+		}
+		this.id = this._entry.id
+	}
+
+	validate_entry = async () => {
+		// If the entry is valid then it have all the necessary fields
+		// as defined by the core validator, hence it is resolved
+		if (this.resolved) return this.resolved
+		try {
+			const obj = await validate(this._entry)
+			this.resolved = true
+		} catch (error) {
+			this.resolved = false
+		}
+		return this.resolved
+	}
+
+	resolve_entry = async () => {
+		await this.validate_entry()
+		if (!this.resolved){
+			const {resolved_entries, invalid_entries} = await this._data_resolver.resolve_entries(
+				{
+					model: this._model,
+					entries: [this]
+				}
+			)
+			if (resolved_entries[this.id]!==undefined){
+				this._entry = resolved_entries[this.id]
+				this.resolved = true
+			} else {
+				throw new Error(`Cannot resolve ${singular_form[this._model]} with id ${this._entry.id}`)
+			}
+		}
+	}
+
+	resolve_parent = async () => {
+		if (!this.resolved) await this.resolve_entry()
+		if (this._parent_model == null) {
+			this._parent = null
+		}else {
+			if (this._parent === undefined){
+				const parent_model = singular_form[this._parent_model]
+				this._parent = new Model(this._parent_model, this._entry[parent_model], this._data_resolver)
+			}
+			await this._parent.resolve_entry()
+		}
+	}
+
+	resolve_children = async ({...filter}) => {
+		if (filter.where!==undefined){
+			filter.where = {
+				and: [
+					filter.where,
+					{
+						[singular_form[this._model]]: this.id
+					}
+				]
+			}
+		}else {
+			filter.where = {
+				[singular_form[this._model]]: this.id
+			}
+		}
+		filter = {
+			limit: 10,
+			...filter
+		}
+		const { entries, count} = await this._data_resolver.filter_metadata({
+			model: this._child_model,
+			filter: filter,
+			parent: this
+		})
+		this._children = entries
+		this._children_count = count
+	}
+
+	entry = async () => {
+		if (!this.resolved) await this.resolve_entry()
+		return this._entry
+	}
+
+	parent = async () => {
+		if (this._parent === undefined) await this.resolve_parent()
+		if (this._parent === null) return null
+		return await this._parent.entry()
+	}
+
+	children = async () => {
+		if (this._children === undefined) await this.resolve_children()
+		return {
+			count: this._children_count,
+			[this._child_model]: this._children
+		}
+	}
+
+	serialize = async () => {
+		const entry = await this.entry()
+		const parent = await (await this.parent()).entry()
+		await this.children()
+		const children = []
+		for (const child of Object.values(this._children)) {
+			children.push(await child.entry())
+		  }
+		entry[singular_form[this._parent_model]] = parent
+		entry[this._child_model] = children
+		return entry
+	}
+
+	update_entry = async (updated_entry) => {
+		this._entry = merge(this._entry, updated_entry)
+		await this.validate_entry()
+	}
+
+	abort = () => {
+		this._data_resolver.abort_controller()
+	}
+}
+
+export class Signature extends Model {
+	resolve_children = async ({limit=10, skip=0, ...filter}={limit:10, skip:0}) => {
+		const start = skip
+		const end = (start + 1) * limit
+		const {results,
+			all_entities} = await this._data_resolver.get_entities_from_signatures([this])
+		this._children_count = results[this.id].length
+		let entities = results[this.id]
+		// if limit = 0 get everything
+		if (limit > 0){
+			entities = results[this.id].slice(start, end)
+		}
+		
+		const { resolved_entries } = await this._data_resolver.resolve_entries({
+				model: this._child_model,
+				entries: entities,
+				parent: this
+			})
+		this._children = entities.map(e=>resolved_entries[e])
+	}
+}
+
+export class Entity extends Model {
+	resolve_children = async ({limit, ...filter}) => {
+		const query = {
+			entities: [this.id],
+			...filter	
+		}
+		if (limit!==0) {
+			query.limit = limit
+		}
+		const { entries,
+			count } = await this._data_resolver.get_signatures_from_entities({query})
+		this._children = entries
+        this._children_count = count
+	}
+}
