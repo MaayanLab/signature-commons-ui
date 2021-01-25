c7d85b8fc90ecee838407f896e5a0ebabd7e704b
erol <je.evangelista04@gmail.com>
2021-01-25 17:38:41 -0500

    Added key

--- components/SearchComponents/Filter.js
+++ components/SearchComponents/Filter.js
@@ -55,6 +55,7 @@ export const Filter = (props) => {
 				<FormGroup>
 						{Object.entries(values).map(([label, count])=>
 							<FormControlLabel
+								key={label}
 								control={<Checkbox checked={checked[label] || false} onChange={onClick} name={label} value={label} />}
 								label={<Typography align="left">{`${label} (${count})`}</Typography>}
 								style={{margin:10}}
