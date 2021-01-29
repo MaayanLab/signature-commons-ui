d74f8d696faa43f05c4ba91f4dff78d896f14d0d
erol <je.evangelista04@gmail.com>
2021-01-28 21:17:42 -0500

    SignatureSearch Search Components

--- components/Search/SignatureSearch.js
+++ components/Search/SignatureSearch.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import {DataResolver, build_where} from '../../connector'
+import {build_where} from '../../connector'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { labelGenerator } from '../../util/ui/labelGenerator'
 import { get_filter, resolve_ids } from './utils'
 import PropTypes from 'prop-types'
 import {SignatureSearchComponent} from './SignatureSearchComponent'
+import Badge from '@material-ui/core/Badge';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
 
 export default class SignatureSearch extends React.PureComponent {
 	constructor(props){
 		super(props)
 		this.state = {
 			entries: null,
+			input: null,
 			model: "resources",
-			resolver: new DataResolver(),
-			searching: false
+			searching: false,
+			resolving: false,
+			query: {},
+			error: null
 		}
 	}
 
-	search_resources = async () => {
+	search_resources = async (searching=null) => {
 		try {
-			this.state.resolver.abort_controller()
-			this.state.resolver.controller()
+			this.props.resolver.abort_controller()
+			this.props.resolver.controller()
 			const {schemas} = this.props
 			const {search: filter_string} = this.props.location
 			const query = get_filter(filter_string)
@@ -36,7 +42,7 @@ export default class SignatureSearch extends React.PureComponent {
 				limit, skip, order
 			}
 			if (where) filter["where"] = where
-			const {entries: results, count} = await this.state.resolver.filter_metadata({
+			const {entries: results, count} = await this.props.resolver.filter_metadata({
 				model: this.state.model,
 				filter,
 			})
@@ -56,7 +62,7 @@ export default class SignatureSearch extends React.PureComponent {
 				page: skip/limit,
 				perPage: limit,
 				query,
-				searching: false,
+				searching: searching !== null ? searching : this.props.match.params.enrichment_id!==undefined,
 				paginate: false,
 			})
 		} catch (error) {
@@ -75,8 +81,8 @@ export default class SignatureSearch extends React.PureComponent {
 			const {schemas} = this.props
 			const input = {...this.state.input}
 			
-			this.state.resolver.abort_controller()
-			this.state.resolver.controller()
+			this.props.resolver.abort_controller()
+			this.props.resolver.controller()
 			
 			const entities_labels = []
 			for (const entities of Object.values(input)){
@@ -97,7 +103,7 @@ export default class SignatureSearch extends React.PureComponent {
 					or
 				}
 			}
-			const {entries: results} = await this.state.resolver.filter_metadata({
+			const {entries: results} = await this.props.resolver.filter_metadata({
 				model: "entities",
 				filter,
 			})
@@ -105,8 +111,7 @@ export default class SignatureSearch extends React.PureComponent {
 			for (const c of Object.values(results)){
 				const entry = await c.entry()
 				const e = labelGenerator(await entry,
-					schemas,
-					"#" + this.props.preferred_name[this.state.model] +"/")
+					schemas)
 				const label = e.info.name.text
 				for (const field of Object.keys(input)){
 					if (input[field][label]!==undefined){
@@ -140,8 +145,10 @@ export default class SignatureSearch extends React.PureComponent {
 					}
 				}
 			}
+			
 			this.setState({
-				input
+				input,
+				resolving: false,
 			})
 		} catch (error) {
 			console.error(error)
@@ -158,7 +165,8 @@ export default class SignatureSearch extends React.PureComponent {
 				}
 			}
 			return({
-				field
+				field,
+				resolving: true
 			})
 		}, () => this.resolve_entities())
 	  }
@@ -194,16 +202,258 @@ export default class SignatureSearch extends React.PureComponent {
 		})
 	}
 
-	componentDidMount = () => {
-		const { schemas } = this.props
-		const {type} = this.props.match.params
-		const input = {}
-		if (type === "Overlap"){
-			input.entities = {}
-		} else {
-			input.up_entities = {}
-			input.down_entities = {}
+	badge = (props) => {
+		return <Badge badgeContent={props.score} color="error"/>
+	}
+
+	get_signature_entities = async (signature_id) => {
+		try {
+			this.props.resolver.abort_controller()
+			this.props.resolver.controller()
+			const {resolved_entries} = await this.props.resolver.resolve_entries({model: "signatures", entries: [signature_id]})
+			const signature = resolved_entries[signature_id]
+			if (signature === undefined) return null
+			else {
+				const children = await signature.children({limit: 0})
+				const input_entities = []
+				const query_entities = []
+				for (const c of children.entities){
+					const entry = labelGenerator(c, this.props.schemas)
+					query_entities.push(c.id)
+					input_entities.push({
+						label: entry.info.name.text,
+						id: [c.id],
+						type: "valid"
+					})
+				}
+				const input = {
+					entities: input_entities
+				}
+				const query = {
+					entities: query_entities,
+					signature_id,
+					input
+				}
+				this.setState({input})
+				return query
+				
+			}
+		} catch (error) {
+			console.error(error)
 		}
+			
+	}
+	
+	get_enrichment = async (enrichment_id) => {
+		let enrichment = this.props.resolver.get_enrichment(enrichment_id)
+		if (enrichment === undefined){
+			const query = await this.get_signature_entities(enrichment_id)
+			// check if enrichment id is a signature instead
+			if (query === null) {
+				const input = {}
+				if (this.props.match.params.type === "Overlap"){
+					input.entities = {}
+				} else {
+					input.up_entities = {}
+					input.down_entities = {}
+				}
+				this.setState({
+					error: "Invalid signature",
+					input,
+				}, ()=>this.search_resources(false))
+				return null
+			}
+			else {
+				const {entities, signature_id, input} = query
+				enrichment = await this.enrichment({entities, signature_id, input, input_type: "set"})
+			}
+		}
+		return enrichment
+	}
+
+	process_enrichment = async (enrichment) => {
+		try {
+			const { entries } = enrichment
+			const {resolved_entries: signatures} = await this.props.resolver.resolve_entries({model: "signatures", entries})
+			const libids = []
+			for (const e of Object.values(signatures)){
+				libids.push((await e.entry()).library)
+			} 
+			const {resolved_entries: libraries} = await this.props.resolver.resolve_entries({model: "libraries", entries: libids})
+			const libraries_count = {}
+			const libraries_children = {}
+			const resources = {}
+			const resources_count = {}
+			const resources_children = {}
+			
+			let unresolved_entitites = []
+			for (const unresolved_entry of entries ) {
+				const {overlap, ...scores} = unresolved_entry.scores
+				unresolved_entitites = [...unresolved_entitites, ...overlap]
+			}
+			const {resolved_entries: entities} = await this.props.resolver.resolve_entries({model: "entities", entries: unresolved_entitites})
+			
+			for (const unresolved_entry of entries ) {
+				if (signatures[unresolved_entry.id]!==undefined){
+					const signature = signatures[unresolved_entry.id]
+					const {overlap, ...scores} = unresolved_entry.scores
+					signature.update_entry({
+						id: unresolved_entry.id,
+						scores
+					})
+					await signature.set_children(overlap)
+					signatures[signature.id] = signature
+					
+					// Get Parents for counting
+					const library = await signature.parent_object()
+					const resource = await library.parent_object()
+
+					if (resources[resource.id] === undefined) resources[resource.id] = resource
+
+					if (libraries_count[library.id]===undefined) libraries_count[library.id]={"Results": 0}
+					if (resources_count[resource.id]===undefined) resources_count[resource.id]={"Results": 0}
+
+					if (libraries_children[library.id]===undefined) libraries_children[library.id]=[]
+					if (resources_children[resource.id]===undefined) resources_children[resource.id]=[]
+					libraries_children[library.id].push(signature)
+					resources_children[resource.id].push(library)
+
+					libraries_count[library.id].Results = libraries_count[library.id].Results + 1
+					library.update_entry({
+						score: libraries_count[library.id]
+					})
+					resources_count[resource.id].Results = resources_count[resource.id].Results + 1
+					resource.update_entry({
+						score: resources_count[resource.id]
+					})
+					libraries[library.id] = library
+					resources[resource.id] = resource
+				}
+			}
+			for (const [id, children] of Object.entries(resources_children)){
+				const resource = resources[id]
+				await resource.set_children(children)
+			}
+			for (const [id, children] of Object.entries(libraries_children)){
+				const library = libraries[id]
+				await library.set_children(children)
+			}
+			const {limit=10, skip=0} = this.state.query
+			const resource_entries = []
+			for (const r of Object.values(resources)){
+				resource_entries.push(await r.entry())
+			}
+			const sorted_entries = resource_entries.sort((a,b)=>b.score.Results - a.score.Results)
+			const new_entries = []
+			for (const entry of sorted_entries){
+				const e = labelGenerator(entry,
+					this.props.schemas,
+					`#${this.props.location.pathname}/${this.props.preferred_name.resources}/`)
+				if (entry.score !== undefined && entry.score.Results !== undefined){
+					e["RightComponents"] = [
+						{
+							component: this.badge,
+							props: {
+								score: entry.score.Results,
+								GridProps: {
+									style: {
+										textAlign: "right",
+										marginRight: 5
+									}
+								}
+							}
+						}
+					]
+				}	
+				new_entries.push(e)
+			}
+			let input = enrichment.input
+			if (input === undefined){
+				input = {}
+				if (this.props.match.params.type === "Overlap"){
+					input.entities = {}
+				} else {
+					input.up_entities = {}
+					input.down_entities = {}
+				}
+			}
+			this.setState({
+				searching: false,
+				entries: new_entries,
+				count: resource_entries.length,
+				resources,
+				input
+			})
+		} catch (error) {
+			console.error(error)
+		}
+	}
+	enrichment = async (query) => {
+		try {
+			this.props.resolver.abort_controller()
+			this.props.resolver.controller()
+			const {enrichment_id, ...enrichment} =  await this.props.resolver.enrichment(query, this.state.input)
+			if (query.signature_id===undefined){
+				this.props.history.push({
+					pathname: `${this.props.nav.SignatureSearch.endpoint}/${this.props.match.params.type}/${enrichment_id}`,
+					state: {input: this.state.input}
+				})
+			}else {
+				return enrichment
+			}
+			
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	performSigSearch = async () => {
+		const input = this.state.input
+		const query = {
+			input_type: input.up_entities !== undefined ? "up_down": "set"
+		}
+		for (const [field, entities] of Object.entries(input)){
+			query[field] = []
+			for (const e of Object.values(entities)){
+				query[field] = [...query[field], ...e.id]
+			}
+		}
+		this.setState({
+			searching: true,
+			query
+		}, ()=>this.enrichment(this.state.query))
+	}
+
+	componentDidUpdate = async (prevProps) => {
+		const prevEnrichmentID = prevProps.match.params.enrichment_id
+		const currEnrichmentID = this.props.match.params.enrichment_id
+		if (prevEnrichmentID !== currEnrichmentID) {
+			let input = {}
+				if (this.props.match.params.type === "Overlap"){
+					input.entities = {}
+				} else {
+					input.up_entities = {}
+					input.down_entities = {}
+				}
+			if (currEnrichmentID !== undefined){
+				if ((this.props.location.state|| {}).input !== undefined) input = this.props.location.state.input
+				this.setState({
+					input, 
+					searching: true
+				}, async () => {
+					const enrichment = await this.get_enrichment(this.props.match.params.enrichment_id)
+					if (enrichment) await this.process_enrichment(enrichment)
+				})
+			}else {
+				await this.search_resources()
+				this.setState({input})
+			}
+		}	
+	}
+	
+	componentDidMount = async () => {
+		const { schemas } = this.props
+		const {type, enrichment_id} = this.props.match.params	
 		const entity_schemas = schemas.filter(s=>s.type==="entity")
 		const titles = []
 		const synonyms = []
@@ -222,68 +472,117 @@ export default class SignatureSearch extends React.PureComponent {
 				value: v.navName,
 			})
 		}
+		await this.search_resources()
+		let input
+		if (enrichment_id === undefined){
+			input = {}
+			if (type === "Overlap"){
+				input.entities = {}
+			} else {
+				input.up_entities = {}
+				input.down_entities = {}
+			}
+			this.setState({
+				titles,
+				synonyms,
+				search_tabs,
+				input,
+				searching: false
+			})
+		} else {
+			this.setState({
+				titles,
+				synonyms,
+				search_tabs,
+				searching: true
+			}, async () => {
+				const enrichment = await this.get_enrichment(this.props.match.params.enrichment_id)
+				if (enrichment) await this.process_enrichment(enrichment)
+			})
+		}
+	}
+
+	handleSnackBarClose = (event, reason) => {
 		this.setState({
-			titles,
-			synonyms,
-			search_tabs,
-			input
-		}, ()=> this.search_resources())
+			error: null
+		}, ()=>this.props.history.push({
+			pathname: `${this.props.nav.SignatureSearch.endpoint}`
+		}))
 	}
 
 	render = () => {
-		if (this.state.entries === null) return <CircularProgress />
+		if (this.state.input === null) return <CircularProgress />
 		else {
+			const disabled = Object.keys(this.state.input.entities || {}).length === 0 || (Object.keys(this.state.input.up_entities || {}) === 0 && Object.keys(this.state.input.down_entities || {}).length === 0)
 			return (
-				<SignatureSearchComponent 
-					searching={this.state.searching}
-					entries={this.state.entries}
-					PaginationProps={{
-						page: this.state.page,
-						rowsPerPage: this.state.perPage,
-						count:  this.state.count,
-						onChangePage: (event, page) => this.handleChangePage(event, page),
-						onChangeRowsPerPage: this.handleChangeRowsPerPage,
-					}}
-					ModelTabProps={{
-						tabs: this.state.vertical_tab_props,
-						value: this.props.preferred_name[this.props.model],
-						tabsProps:{
-							orientation: "vertical",
-							TabIndicatorProps: {
-								style: {"left": 0}
-							}
-						},
-						handleChange: this.handleTabChange
-					}}
-					TabProps={{
-						tabs: [
-							{
-								label: this.props.preferred_name[this.state.model],
-								value: this.props.preferred_name[this.state.model],
-								count: this.state.count
-							}
-						],
-						value: this.props.preferred_name[this.state.model],
-						tabsProps:{
-							centered: true
-						},
-					}}
-					SearchTabProps={{
-						tabs: this.state.search_tabs,
-						value:"Signature Search",
-						tabsProps:{
-							centered: true
-						},
-						handleChange: this.handleTabChange
-					}}
-					TextFieldSuggestProps={{
-						input: this.state.input,
-						onAdd: this.onAddEntity,
-						onSubmit: this.onAddEntity,
-						onDelete: this.onDeleteEntity,
-						onSuggestionClick: this.onSuggestionClickEntity,
-					}}
-				/>
+				<React.Fragment>
+					<Snackbar open={this.state.error!==null}
+						anchorOrigin={{ vertical:"top", horizontal:"right" }}
+						autoHideDuration={6000}
+						onClose={this.handleSnackBarClose}
+						message={this.state.error}
+						action={
+							<IconButton size="small" aria-label="close" color="inherit" onClick={this.handleSnackBarClose}>
+								<span className="mdi mdi-close-box mdi-24px"/>
+							</IconButton>
+						}
+					/>
+					<SignatureSearchComponent 
+						searching={this.state.searching}
+						resolving={this.state.resolving}
+						entries={this.state.entries}
+						PaginationProps={{
+							page: this.state.page,
+							rowsPerPage: this.state.perPage,
+							count:  this.state.count,
+							onChangePage: (event, page) => this.handleChangePage(event, page),
+							onChangeRowsPerPage: this.handleChangeRowsPerPage,
+						}}
+						ModelTabProps={{
+							tabs: this.state.vertical_tab_props,
+							value: this.props.preferred_name[this.props.model],
+							tabsProps:{
+								orientation: "vertical",
+								TabIndicatorProps: {
+									style: {"left": 0}
+								}
+							},
+							handleChange: this.handleTabChange
+						}}
+						TabProps={{
+							tabs: [
+								{
+									label: this.props.preferred_name[this.state.model],
+									value: this.props.preferred_name[this.state.model],
+									count: this.state.count
+								}
+							],
+							value: this.props.preferred_name[this.state.model],
+							tabsProps:{
+								centered: true
+							},
+						}}
+						SearchTabProps={{
+							tabs: this.state.search_tabs,
+							value:"Signature Search",
+							tabsProps:{
+								centered: true
+							},
+							handleChange: this.handleTabChange
+						}}
+						TextFieldSuggestProps={{
+							input: this.state.input,
+							onAdd: this.onAddEntity,
+							onSubmit: this.performSigSearch,
+							onDelete: this.onDeleteEntity,
+							onSuggestionClick: this.onSuggestionClickEntity,
+							examples: this.props.examples,
+							type: this.props.match.params.type,
+							disabled,
+						}}
+					/>
+				</React.Fragment>
+				
 			)
 		}
 	}
@@ -315,5 +614,20 @@ SignatureSearch.propTypes = {
 		resource_name_to_id: PropTypes.objectOf(PropTypes.string),
 		lib_to_resource: PropTypes.objectOf(PropTypes.string),
 		resource_to_lib: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
-	})
+	}),
+	examples: PropTypes.arrayOf(
+		PropTypes.oneOf([
+			PropTypes.shape({
+				type: PropTypes.oneOf(["Overlap"]),
+				entities: PropTypes.string,
+				label: PropTypes.string
+			}),
+			PropTypes.shape({
+				type: PropTypes.oneOf(["Rank"]),
+				up_entities: PropTypes.string,
+				down_entities: PropTypes.string,
+				label: PropTypes.string
+			})
+		])
+	)
 }
--- components/Search/SignatureSearchComponent.js
+++ components/Search/SignatureSearchComponent.js
@@ -7,22 +7,41 @@ import {ResultsTab} from '../MetadataPage/ResultsTab'
 import {DataTable} from '../DataTable'
 import { Typography, Divider } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress'
 
+const Examples = (props) => {
+	const {examples, type, onAdd} = props
+	const example_buttons = []
+	for (const ex of examples){
+		if (ex.type === type){
+			example_buttons.push(
+				<Button variant="text" color="primary" key={ex.label} onClick={()=>{
+					if (type === "Overlap") {
+						onAdd(ex.entities, "entities")
+					} else {
+						onAdd(ex.up_entities, "up_entities")
+						onAdd(ex.down_entities, "down_entities")
+					}
+				}}>
+					{ex.label}
+				</Button>
+			)
+		}
+	}
+	return example_buttons
+}
 
 export const SignatureSearchComponent = (props) => {
 	const {
 		searching=false,
-		search_terms=[],
-		search_examples=[],
 		chipRenderer,
-		onSearch,
 		entries,
 		DataTableProps,
 		PaginationProps,
 		SearchTabProps,
 		TabProps,
 		TextFieldSuggestProps,
-		field,
+		resolving
 	} = props
 
 
@@ -53,7 +72,7 @@ export const SignatureSearchComponent = (props) => {
 							<TextFieldSuggest {...TextFieldSuggestProps}
 									input={Object.values(TextFieldSuggestProps.input.entities)}
 									onAdd={(value)=>TextFieldSuggestProps.onAdd(value, "entities")}
-									onSubmit={(value)=>TextFieldSuggestProps.onSubmit(value, "entities")}
+									onSubmit={(value)=>TextFieldSuggestProps.onAdd(value, "entities")}
 									onDelete={(value)=>TextFieldSuggestProps.onDelete(value, "entities")}
 									onSuggestionClick={(value, selected)=>TextFieldSuggestProps.onSuggestionClick(value, selected, "entities")}
 							/>:
@@ -61,59 +80,54 @@ export const SignatureSearchComponent = (props) => {
 								<TextFieldSuggest {...TextFieldSuggestProps}
 									input={Object.values(TextFieldSuggestProps.input.up_entities)}
 									onAdd={(value)=>TextFieldSuggestProps.onAdd(value, "up_entities")}
-									onSubmit={(value)=>TextFieldSuggestProps.onSubmit(value, "up_entities")}
+									onSubmit={(value)=>TextFieldSuggestProps.onAdd(value, "up_entities")}
 									onDelete={(value)=>TextFieldSuggestProps.onDelete(value, "up_entities")}
 									onSuggestionClick={(value, selected)=>TextFieldSuggestProps.onSuggestionClick(value, selected, "up_entities")}
 								/>
 								<TextFieldSuggest {...TextFieldSuggestProps}
 									input={Object.values(TextFieldSuggestProps.input.down_entities)}
-									onAdd={(value)=>onAdd(value, "down_entities")}
-									onSubmit={(value)=>onSubmit(value, "down_entities")}
-									onDelete={(value)=>onDelete(value, "down_entities")}
-									onSuggestionClick={(value, selected)=>onSuggestionClick(value, selected, "down_entities")}
+									onAdd={(value)=>TextFieldSuggestProps.onAdd(value, "down_entities")}
+									onSubmit={(value)=>TextFieldSuggestProps.onAdd(value, "down_entities")}
+									onDelete={(value)=>TextFieldSuggestProps.onDelete(value, "down_entities")}
+									onSuggestionClick={(value, selected)=>TextFieldSuggestProps.onSuggestionClick(value, selected, "down_entities")}
 								/>
 							</React.Fragment>
 						}
 						
 					</Grid>
 					<Grid item xs={12} align="center">
-						{searching ? 
-							<Button variant="contained" color="primary" size="small" style={{marginTop:10}}>
-								<span class="mdi mdi-loading mdi-spin mdi-24px" /><Typography align={"center"}>Searching...</Typography>
+						{searching || resolving ? 
+							<Button variant="contained" color="primary" size="small" disabled style={{marginTop:10}}>
+								<span className="mdi mdi-loading mdi-spin mdi-24px" /><Typography align={"center"}>Searching...</Typography>
 							</Button>
 						:
-							<Button variant="contained" color="primary" size="small" style={{marginTop:10}}>
+							<Button variant="contained" color="primary" size="small" disabled={TextFieldSuggestProps.disabled} style={{marginTop:10}} onClick={()=>TextFieldSuggestProps.onSubmit()}>
 								<span className="mdi mdi-magnify mdi-24px" /><Typography align={"center"}>Search</Typography>
 							</Button>
 						}
 					</Grid>
 				</Grid>
 				<Typography align={"center"}  style={{marginTop:10}}>
-				{search_examples.map((v,i)=>(
-					<React.Fragment>
-						<Button variant="text" color="primary" onClick={()=>{
-							if (search_terms.indexOf(v)<0) onSearch([...search_terms, v])
-						}}>
-							{v}
-						</Button>
-						{i === search_examples.length - 1 ? null: " / "}
-					</React.Fragment>
-				))}
+						<Examples {...TextFieldSuggestProps} />
 				</Typography>
 			</Grid>
 			<Grid item xs={12} md={6} lg={7}>
-				<ResultsTab
-					tabsProps={{
-						centered: true,
-					}}
-					{...TabProps}
-				/>
-				<DataTable entries={entries} {...DataTableProps}/>
-				<TablePagination
-					{...PaginationProps}
-					component="div"
-					align="right"
-				/>
+				{ searching? <CircularProgress/>:
+					<React.Fragment>
+						<ResultsTab
+							tabsProps={{
+								centered: true,
+							}}
+							{...TabProps}
+						/>
+						<DataTable entries={entries} {...DataTableProps}/>
+						<TablePagination
+							{...PaginationProps}
+							component="div"
+							align="right"
+						/>
+					</React.Fragment>
+				}
 			</Grid>
 		</Grid>
 	)
@@ -173,28 +187,78 @@ SignatureSearchComponent.propTypes = {
 		tabsProps: PropTypes.object,
 	}).isRequired,
 	TextFieldSuggestProps: PropTypes.shape({
-		input: PropTypes.arrayOf(PropTypes.shape({
-			label: PropTypes.string.isRequired,
-			type: PropTypes.oneOf(["valid", "suggestions", "invalid", "loading", "disabled"]),
-			id: PropTypes.arrayOf(PropTypes.oneOfType([
-				PropTypes.string,
-				PropTypes.number
-			  ])),
-			suggestions: PropTypes.arrayOf(PropTypes.shape({
-				label: PropTypes.string.isRequired,
-				type: PropTypes.oneOf(["valid", "suggestions", "invalid", "loading", "disabled"]),
-				id: PropTypes.oneOfType([
-					PropTypes.string,
-					PropTypes.number
-				  ]),     
-			})),
-			gridColumnProps: PropTypes.object,
-			gridRowProps: PropTypes.object,
-			avatarProps: PropTypes.object,
-			labelProps: PropTypes.object,
-			chipProps: PropTypes.object,
-			suggestionsProps: PropTypes.object,
-		})).isRequired,
+		input: PropTypes.shape(PropTypes.oneOf([
+			{
+				entities: PropTypes.arrayOf(PropTypes.shape({
+					label: PropTypes.string.isRequired,
+					type: PropTypes.oneOf(["valid", "suggestions", "invalid", "loading", "disabled"]),
+					id: PropTypes.arrayOf(PropTypes.oneOfType([
+						PropTypes.string,
+						PropTypes.number
+					  ])),
+					suggestions: PropTypes.arrayOf(PropTypes.shape({
+						label: PropTypes.string.isRequired,
+						type: PropTypes.oneOf(["valid", "suggestions", "invalid", "loading", "disabled"]),
+						id: PropTypes.oneOfType([
+							PropTypes.string,
+							PropTypes.number
+						  ]),     
+					})),
+					gridColumnProps: PropTypes.object,
+					gridRowProps: PropTypes.object,
+					avatarProps: PropTypes.object,
+					labelProps: PropTypes.object,
+					chipProps: PropTypes.object,
+					suggestionsProps: PropTypes.object,
+				}))
+			},
+			{
+				up_entities: PropTypes.arrayOf(PropTypes.shape({
+					label: PropTypes.string.isRequired,
+					type: PropTypes.oneOf(["valid", "suggestions", "invalid", "loading", "disabled"]),
+					id: PropTypes.arrayOf(PropTypes.oneOfType([
+						PropTypes.string,
+						PropTypes.number
+					  ])),
+					suggestions: PropTypes.arrayOf(PropTypes.shape({
+						label: PropTypes.string.isRequired,
+						type: PropTypes.oneOf(["valid", "suggestions", "invalid", "loading", "disabled"]),
+						id: PropTypes.oneOfType([
+							PropTypes.string,
+							PropTypes.number
+						  ]),     
+					})),
+					gridColumnProps: PropTypes.object,
+					gridRowProps: PropTypes.object,
+					avatarProps: PropTypes.object,
+					labelProps: PropTypes.object,
+					chipProps: PropTypes.object,
+					suggestionsProps: PropTypes.object,
+				})),
+				down_entities: PropTypes.arrayOf(PropTypes.shape({
+					label: PropTypes.string.isRequired,
+					type: PropTypes.oneOf(["valid", "suggestions", "invalid", "loading", "disabled"]),
+					id: PropTypes.arrayOf(PropTypes.oneOfType([
+						PropTypes.string,
+						PropTypes.number
+					  ])),
+					suggestions: PropTypes.arrayOf(PropTypes.shape({
+						label: PropTypes.string.isRequired,
+						type: PropTypes.oneOf(["valid", "suggestions", "invalid", "loading", "disabled"]),
+						id: PropTypes.oneOfType([
+							PropTypes.string,
+							PropTypes.number
+						  ]),     
+					})),
+					gridColumnProps: PropTypes.object,
+					gridRowProps: PropTypes.object,
+					avatarProps: PropTypes.object,
+					labelProps: PropTypes.object,
+					chipProps: PropTypes.object,
+					suggestionsProps: PropTypes.object,
+				}))
+			}
+		])).isRequired,
 		onSubmit: PropTypes.func.isRequired,
 		onAdd: PropTypes.func.isRequired,
 		onDelete: PropTypes.func.isRequired,
