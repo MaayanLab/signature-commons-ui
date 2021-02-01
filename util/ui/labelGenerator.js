4cc4aaee2051f9b0226046a99e036f55b059b310
erol <je.evangelista04@gmail.com>
2021-02-01 16:56:19 -0500

    Added new things for label resolver

--- util/ui/labelGenerator.js
+++ util/ui/labelGenerator.js
@@ -88,6 +88,68 @@ export const value_by_type = {
 		return { text: val, label }
 	  }
   },
+  'download': ({ label, prop, data }) => {
+    const props = {}
+    for (const [k,v] of Object.entries(prop.props)){
+      const val = value_by_type[v.type]({label: k, prop: v, data})
+      if (val!==null) props[k] = val
+    }
+    if (Object.keys(props).length ===0) return null
+    else return {label, props}
+  },
+  'prop-array': ({label, prop, data}) => {
+    
+    const props = []
+    for (const i of prop.items){
+      const val = value_by_type[i.type]({label, prop: i, data})
+      if (val!==null){
+        if (i.type === 'text') {
+          props.push(val.text)
+        }else{
+          props.push(val)
+        }
+      }
+    }
+    if (props.length ===0) return null
+    else return props
+  },
+  'prop-object': ({label, prop, data}) => {
+    const props = {}
+    for (const [k,v] of Object.entries(prop.properties)){
+      const val = value_by_type[v.type]({label:k, prop: v, data})
+      if (val!==null){
+        if (v.type==='text'){
+          props[k] = val.text
+        } else {
+          props[k] = val
+        }
+      }
+    }
+    if (Object.keys(props).length ===0) return null
+    else return props
+  },
+  'component': ({label, prop, data}) => (null)
+}
+
+export const props_resolver = (properties, data, info=null) => {
+  if (info===null) info={}
+  for (const [label, props] of Object.entries(properties)) {
+
+  }
+  
+}
+
+export const getName = (data, schemas) => {
+  const schema = findMatchedSchema(data, schemas)
+  if (schema !== null) {
+    for (const prop of Object.values(schema.properties)) {
+      if (prop.visibility && prop.type==="title"){
+        const val = value_by_type[prop.type]({ label: "title", prop, data })
+        if (val!==null) return val.text
+      }
+    }
+  }
+  return null
 }
 
 export const labelGenerator = (data, schemas, endpoint=undefined, highlight=undefined) => {
@@ -105,7 +167,7 @@ export const labelGenerator = (data, schemas, endpoint=undefined, highlight=unde
     const sort_tags = {}
     for (const label of Object.keys(properties)) {
       const prop = properties[label]
-
+      if (prop.component === "download") prop.type = "download"
       if (prop.visibility && prop.visibility > 0 && objectMatch(prop.condition, data) && value_by_type[prop.type]!==undefined) {
         const val = value_by_type[prop.type]({ label, prop, data })
         if (prop.synonyms){
@@ -156,11 +218,18 @@ export const labelGenerator = (data, schemas, endpoint=undefined, highlight=unde
         if (prop.type === "array") {
           // TODO: Update schemas so it has list
           if (val !== null) {
-            keywords[label] = {
-              label,
-              value: val.object,
-              icon: prop.MDI_Icon || 'mdi-tag-multiple',
+            if (val !== null) {
+              keywords[label] = {
+                label,
+                value: val.object,
+                icon: prop.MDI_Icon || 'mdi-tag-multiple',
+              }
             }
+          }
+        }
+        if (prop.type === "download") {
+          if (val !== null) {
+            info.download = val
           }
         }
         if (prop.type==="text") {
@@ -179,7 +248,7 @@ export const labelGenerator = (data, schemas, endpoint=undefined, highlight=unde
     tags = tags.sort((a, b) => a.priority - b.priority)
     if (Object.keys(scores).length > 0) info.scores = scores
     info.tags = tags || []
-    info.download = download || []
+    // info.download = download || []
     info.keywords = keywords
     return { data, info, schema, sort_tags }
   }
