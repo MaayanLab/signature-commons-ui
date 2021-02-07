d6713b05da7f781ff538035a61a8c8bfc8e25101
erol <je.evangelista04@gmail.com>
2021-02-06 20:08:17 -0500

    Fixed app bar

--- components/Base/Header.js
+++ components/Base/Header.js
@@ -38,6 +38,8 @@ const styles = (theme) => ({
   },
   menuItem: {
     color: 'inherit',
+    paddingLeft: 30,
+    paddingRight: 30
   },
   paper: {
     padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
@@ -61,7 +63,9 @@ export function Nav(props) {
           className={classes.menuItem}
           href={`#${ui_values.nav.MetadataSearch.endpoint || '/MetadataSearch'}`}
         >
-          {ui_values.nav.MetadataSearch.navName || ui_values.nav.MetadataSearch.endpoint.substring(1).replace(/([a-z])([A-Z])/g, '$1 $2').replace(/_/g, ' ')}
+          <Typography variant={'button'} style={{fontSize: 20}}>
+            {ui_values.nav.MetadataSearch.navName || ui_values.nav.MetadataSearch.endpoint.substring(1).replace(/([a-z])([A-Z])/g, '$1 $2').replace(/_/g, ' ')}
+          </Typography>
         </ListItemLink> : null
       }
       {ui_values.nav.SignatureSearch.active ?
@@ -70,7 +74,9 @@ export function Nav(props) {
           className={ classes.menuItem}
           href={`#${ui_values.nav.SignatureSearch.endpoint || '/SignatureSearch'}`}
         >
-          {ui_values.nav.SignatureSearch.navName || ui_values.nav.SignatureSearch.endpoint.substring(1).replace(/([a-z])([A-Z])/g, '$1 $2').replace(/_/g, ' ')}
+          <Typography variant={'button'} style={{fontSize: 20}}>
+            {ui_values.nav.SignatureSearch.navName || ui_values.nav.SignatureSearch.endpoint.substring(1).replace(/([a-z])([A-Z])/g, '$1 $2').replace(/_/g, ' ')}
+          </Typography>
         </ListItemLink> : null
       }
       {ui_values.nav.Resources.active ?
@@ -79,30 +85,27 @@ export function Nav(props) {
           className={ classes.menuItem}
           href={`#${ui_values.nav.Resources.endpoint}`}
         >
-          {ui_values.nav.Resources.navName || ui_values.nav.Resources.endpoint.substring(1).replace(/([a-z])([A-Z])/g, '$1 $2').replace(/_/g, ' ')}
-        </ListItemLink> : null
-      }
-      {ui_values.about !== undefined ?
-        <ListItemLink
-          selected={location.pathname === '/About'}
-          className={ classes.menuItem}
-          href={'#/About'}
-        >
-          {'About'}
+          <Typography variant={'button'} style={{fontSize: 20}}>
+            {ui_values.nav.Resources.navName || ui_values.nav.Resources.endpoint.substring(1).replace(/([a-z])([A-Z])/g, '$1 $2').replace(/_/g, ' ')}
+          </Typography>
         </ListItemLink> : null
       }
       <ListItemLink
         className={ classes.menuItem}
         href={'https://appyters.maayanlab.cloud/Drugmonizome_ML/'}
       >
-        {'Drugmonizome ML'}
+          <Typography variant={'button'} style={{fontSize: 20}}>
+            Drugmonizome ML
+          </Typography>
       </ListItemLink>
       <ListItemLink
         selected={location.pathname === '/API'}
         className={ classes.menuItem}
         href={'#/API'}
       >
-        {'API'}
+         <Typography variant={'button'} style={{fontSize: 20}}>
+            API
+          </Typography>
       </ListItemLink>
     </React.Fragment>
   )
@@ -127,10 +130,10 @@ class Header extends React.Component {
     const { staticContext, classes, ...rest } = this.props
     return (
       <header {...this.props.ui_values.header_info.header_props}>
-        <AppBar position="static" color="primary">
+        <AppBar position="static" color="primary" style={{height: 80, paddingTop: 5, paddingBottom: 5}}>
           <Toolbar>
             <Hidden smDown>
-              <Typography variant="h4" color="inherit" className={classes.grow}>
+              <Typography variant="h3" color="inherit" className={classes.grow}>
                 <Link
                   to="/"
                   className={classes.header}
@@ -150,7 +153,7 @@ class Header extends React.Component {
               </Button>
               {[this.props.ui_values.nav.MetadataSearch.endpoint, this.props.ui_values.nav.SignatureSearch.endpoint + '/Overlap', this.props.ui_values.nav.SignatureSearch.endpoint + '/Rank'].indexOf(this.props.location.pathname) > -1 ?
                 <div className={classes.grow}/> :
-                <Typography variant="h4" color="inherit" className={classes.grow}>
+                <Typography variant="h3" color="inherit" className={classes.grow}>
                   <Link
                     to="/"
                     className={classes.header}
@@ -170,7 +173,7 @@ class Header extends React.Component {
                   onClick={this.toggleDrawer}
                   onKeyDown={this.toggleDrawer}
                 >
-                  <List>
+                  <List disablePadding>
                     <Nav classes={classes} {...rest}/>
                   </List>
                 </div>
@@ -179,11 +182,11 @@ class Header extends React.Component {
           </Toolbar>
         </AppBar>
         {paths.length <= 3 ? <div style={{height:25}}/> : (
-            <Breadcrumbs separator={<span className="mdi mdi-arrow-right-bold-circle-outline"/>} aria-label="breadcrumb" component={'div'} style={{marginTop:5, marginLeft: 10, height: 20}}>
+            <Breadcrumbs separator={<span className="mdi mdi-arrow-right-bold-circle-outline"/>} aria-label="breadcrumb" component={'div'} style={{marginTop:10, marginLeft: 20, height: 20}}>
               {paths.slice(1).map((path, i) => {
                 const href = paths.slice(0, i + 2).join('/')
                 return (
-                  <Typography color={'inherit'} key={href}>
+                  <Typography color={'inherit'} key={href} variant={"caption"}>
                     <Link
                       key={href}
                       to={href}
