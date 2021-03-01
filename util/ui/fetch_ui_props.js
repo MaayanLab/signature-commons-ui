82784b9418327ade817788c1bd1e22d7bba1a3e6
erol <je.evangelista04@gmail.com>
2021-03-01 13:19:36 -0500

    refactor ui initialization

---
+++ util/ui/fetch_ui_props.js
@@ -0,0 +1,84 @@
+import { fetch_meta, fetch_meta_post } from '../fetch/meta'
+import merge from 'deepmerge'
+import { makeTemplate } from './makeTemplate'
+import {defaultUIValues, defaultTheme} from './ui_values'
+import { createMuiTheme } from '@material-ui/core'
+import {fill_palette} from './fill_palette'
+
+// getSearchFilters.js
+export const getSearchFilters = async () => {
+	const { response } = await fetch_meta_post({
+		endpoint: '/schemas/find',
+		body: {
+		  filter: {
+			where: {
+			  'meta.$validator': '/dcic/signature-commons-schema/v6/meta/schema/counting.json',
+			  'meta.type': 'filter'
+			},
+		  },
+		},
+	  })
+	const filters = {}
+	for (const i of response){
+		const model = i.meta.model
+		if (filters[model] === undefined) filters[model] = []
+		filters[model].push(i["meta"])
+	}
+	return filters
+}
+
+const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray
+
+export const get_ui_values = async () => {
+	const { response: ui_val } = await fetch_meta_post({
+	  endpoint: '/schemas/find',
+	  body: {
+		filter: {
+		  where: {
+			'meta.$validator': '/dcic/signature-commons-schema/v5/meta/schema/landing-ui.json',
+			'meta.landing': true,
+		  },
+		},
+	  },
+	})
+	const values = ui_val.length > 0 ? ui_val[0].meta.content : {}
+	const ui_values = merge(defaultUIValues, values, { arrayMerge: overwriteMerge })
+	if (ui_values.background_props.style && ui_values.background_props.style.backgroundImage) {
+		ui_values.background_props.style.backgroundImage = 'url(' + makeTemplate(ui_values.background_props.style.backgroundImage, {}) + ')'
+	  }
+	return ui_values
+  }
+
+export const get_theme = (theme_mod) => {
+	const theme = createMuiTheme(merge(defaultTheme, theme_mod))
+	theme.shadows[4] = theme.shadows[0]
+	// Get variables for offsetting colors
+    const tonalOffset = theme.palette.tonalOffset
+    const contrastThreshold = theme.palette.contrastThreshold
+    // fill theme
+    theme.palette = Object.entries(theme.palette).reduce((acc, [key, val]) => {
+		if (val.main !== undefined) {
+		  acc[key] = fill_palette(val, tonalOffset, contrastThreshold)
+		} else {
+		  acc[key] = val
+		}
+		return acc
+	  }, {})
+	
+	return theme
+}
+
+export const get_search_models = async () => {
+	const search_models = []
+	for (const model in ["resources", "libraries", "signatures", "entities"]){
+		const { response } = await fetch_meta({ endpoint: `/${model}/count`})
+ 		if (response.count) search_models.push(model)
+	}
+	return search_models
+}
+
+export const get_initial_props = async () => {
+	const {theme_mod, ...ui_values} = await get_ui_values()
+	const theme = await get_theme(theme_mod)
+	return { ui_values, theme_mod }
+}
--- util/ui/ui_values.js
+++ util/ui/ui_values.js
@@ -1,7 +1,11 @@
-import merge from 'deepmerge'
-import { makeTemplate } from './makeTemplate'
+import blue from '@material-ui/core/colors/blue'
+import red from '@material-ui/core/colors/red'
+import amber from '@material-ui/core/colors/amber'
+import grey from '@material-ui/core/colors/grey'
+import green from '@material-ui/core/colors/green'
+import yellow from '@material-ui/core/colors/yellow'
 
-const default_values = {
+export const defaultUIValues = {
   'landing': {
     font_families: [
       'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap',
@@ -29,26 +33,56 @@ const default_values = {
       },
     },
     background_props: {},
-    text_1: 'Explore an extensive collection of well-annotated gene-sets and signatures',
-    text_2: 'Search across a broad gathering of perturbations',
-    text_3: 'By',
-    text_4: 'Start using Signature Commons in your project',
-    metadata_placeholder: 'Search over half a million signatures',
-    geneset_placeholder: 'Genes that are regulated in signature or overlap with gene set',
-    up_genes_placeholder: 'Genes that are up-regulated in signature or overlap with gene set',
-    down_genes_placeholder: 'Genes that are down-regulated in signature or overlap with gene set',
-    resource_pie_caption: 'Signatures per Resource',
-    search_terms: ['MCF10A', 'Imatinib', 'ZNF830', 'STAT3', 'Neuropathy'],
     nav: {
       MetadataSearch: {
         active: true,
         endpoint: '/MetadataSearch',
-        navName: "Metadata Search"
+        navName: "Metadata Search",
+        props: {
+          placeholder: 'Search for any term, i.e. drug, side effects, or a disease',
+          examples: ['MCF10A', 'Imatinib', 'ZNF830', 'STAT3', 'Neuropathy'],
+        }
       },
       SignatureSearch: {
         active: false,
         endpoint: '/SignatureSearch',
-        navName: "Signature Search"
+        navName: "Signature Search",
+        props: {
+          entity_strategy: 'upper', // lower, none
+          synonym_strategy: 'none',
+          types: {
+            Overlap: {
+              active: true,
+              placeholder: 'Genes that are regulated in signature or overlap with gene set',
+              switch: 'Gene Set or Full Signature',
+              examples: [
+                {
+                  label: 'Example Crisp Gene Set',
+                  input: {
+                    type: 'Overlap',
+                    geneset: 'UTP14A\nS100A6\nSCAND1\nRRP12\nCIAPIN1\nADH5\nMTERF3\nSPR\nCHMP4A\nUFM1\nVAT1\nHACD3\nRFC5\nCOTL1\nNPRL2\nTRIB3\nPCCB\nTLE1\nCD58\nBACE2\nKDM3A\nTARBP1\nRNH1\nCHAC1\nMBNL2\nVDAC1\nTES\nOXA1L\nNOP56\nHAT1\nCPNE3\nDNMT1\nARHGAP1\nVPS28\nEIF2S2\nBAG3\nCDCA4\nNPDC1\nRPS6KA1\nFIS1\nSYPL1\nSARS\nCDC45\nCANT1\nHERPUD1\nSORBS3\nMRPS2\nTOR1A\nTNIP1\nSLC25A46\nMAL\nEPCAM\nHDAC6\nCAPN1\nTNRC6B\nPKD1\nRRS1\nHP\nANO10\nCEP170B\nIDE\nDENND2D\nCAMK2B\nZNF358\nRPP38\nMRPL19\nNUCB2\nGNAI1\nLSR\nADGRE2\nPKMYT1\nCDK5R1\nABL1\nPILRB\nAXIN1\nFBXL8\nMCF2L\nDBNDD1\nIGHMBP2\nWIPF2\nWFS1\nOGFOD2\nMAPK1IP1L\nCOL11A1\nREG3A\nSERPINA1\nMYCBP2\nPIGK\nTCAP\nCRADD\nELK1\nDNAJB2\nZBTB16\nDAZAP1\nMAPKAPK2\nEDRF1\nCRIP1\nUCP3\nAGR2\nP4HA2',
+                  },
+                }
+              ]
+            },
+            Rank: {
+              active: true,
+              switch: 'Up and Down Gene Sets',
+              up_placeholder: 'Genes that are up-regulated in signature or overlap with gene set',
+              down_placeholder: 'Genes that are down-regulated in signature or overlap with gene set',
+              examples: [
+                {
+                  label: 'Example Up and Down Sets',
+                  input: {
+                    type: 'Rank',
+                    up_geneset: 'UTP14A\nS100A6\nSCAND1\nRRP12\nCIAPIN1\nADH5\nMTERF3\nSPR\nCHMP4A\nUFM1\nVAT1\nHACD3\nRFC5\nCOTL1\nNPRL2\nTRIB3\nPCCB\nTLE1\nCD58\nBACE2\nKDM3A\nTARBP1\nRNH1\nCHAC1\nMBNL2\nVDAC1\nTES\nOXA1L\nNOP56\nHAT1\nCPNE3\nDNMT1\nARHGAP1\nVPS28\nEIF2S2\nBAG3\nCDCA4\nNPDC1\nRPS6KA1\nFIS1\nSYPL1\nSARS\nCDC45\nCANT1\nHERPUD1\nSORBS3\nMRPS2\nTOR1A\nTNIP1\nSLC25A46',
+                    down_geneset: 'MAL\nEPCAM\nHDAC6\nCAPN1\nTNRC6B\nPKD1\nRRS1\nHP\nANO10\nCEP170B\nIDE\nDENND2D\nCAMK2B\nZNF358\nRPP38\nMRPL19\nNUCB2\nGNAI1\nLSR\nADGRE2\nPKMYT1\nCDK5R1\nABL1\nPILRB\nAXIN1\nFBXL8\nMCF2L\nDBNDD1\nIGHMBP2\nWIPF2\nWFS1\nOGFOD2\nMAPK1IP1L\nCOL11A1\nREG3A\nSERPINA1\nMYCBP2\nPIGK\nTCAP\nCRADD\nELK1\nDNAJB2\nZBTB16\nDAZAP1\nMAPKAPK2\nEDRF1\nCRIP1\nUCP3\nAGR2\nP4HA2',
+                  }
+                }
+              ]
+            }
+          }
+        }
       },
       Resources: {
         active: true,
@@ -105,189 +139,74 @@ const default_values = {
     powered: true,
     github: 'https://github.com/dcic/signature-commons-ui',
     github_issues: 'https://github.com/dcic/signature-commons-ui/issues',
-    deactivate_download: true,
-    ChipInputStyle: {
-      'disableUnderline': true,
-    },
-    bar_chart_style: {
-      ResponsiveContainer: {
-        width: '100%',
-        height: 420,
-      },
-      Chart: {
-        margin: {
-          right: 50,
-          left: 50,
-          top: 20,
-          bottom: 20,
-        },
-      },
-      XAxis: {
-        axisLine: false,
-        angle: 45,
-        height: 50,
-        textAnchor: 'start',
-        interval: 0,
-        tick: {
-          fontSize: 10,
-        },
-        tickLine: false,
-      },
-      Bar: {
-        fill: '#75bef5',
-      },
-    },
-    pie_chart_style: {
-      ResponsiveContainer: {
-        width: '100%',
-        height: 500,
-      },
-      Chart: {
-        width: 500,
-        height: 500,
-      },
-      Pie: {
-        fill: '#75bef5',
-      },
-      Text_Label: {
-        fontSize: 12,
-      },
-    },
-    counting_validator: '/dcic/signature-commons-schema/v5/meta/schema/counting.json',
-    ui_schema: '/dcic/signature-commons-schema/v5/meta/schema/ui-schema.json',
-    maxResourcesBeforeCollapse: 60,
-    maxResourcesToShow: 40,
-    showNonResource: true,
-    downloads: {
-      gmt: 'Download gmt file',
-      tsv: 'Download tsv file',
-      geneset: 'Download gene set',
-      ranked: 'Download ranked signature',
-      signature_json: 'Download signature as json',
-      library_json: 'Download library as json',
-      resource_json: 'Download resource as json',
-      sigcom: true,
-      enrichr: true,
-    },
-    helper_tooltip: {
-      'term1': 'imatinib',
-      'term2': 'stat3',
-      'term3': 'Disease: Neuropathy',
-      'term4': 'PMID: 123456',
-    },
-    overlap_search: true,
-    rank_search: true,
-    examples: [
-      {
-        label: 'Example Crisp Gene Set',
-        input: {
-          type: 'Overlap',
-          geneset: 'UTP14A\nS100A6\nSCAND1\nRRP12\nCIAPIN1\nADH5\nMTERF3\nSPR\nCHMP4A\nUFM1\nVAT1\nHACD3\nRFC5\nCOTL1\nNPRL2\nTRIB3\nPCCB\nTLE1\nCD58\nBACE2\nKDM3A\nTARBP1\nRNH1\nCHAC1\nMBNL2\nVDAC1\nTES\nOXA1L\nNOP56\nHAT1\nCPNE3\nDNMT1\nARHGAP1\nVPS28\nEIF2S2\nBAG3\nCDCA4\nNPDC1\nRPS6KA1\nFIS1\nSYPL1\nSARS\nCDC45\nCANT1\nHERPUD1\nSORBS3\nMRPS2\nTOR1A\nTNIP1\nSLC25A46\nMAL\nEPCAM\nHDAC6\nCAPN1\nTNRC6B\nPKD1\nRRS1\nHP\nANO10\nCEP170B\nIDE\nDENND2D\nCAMK2B\nZNF358\nRPP38\nMRPL19\nNUCB2\nGNAI1\nLSR\nADGRE2\nPKMYT1\nCDK5R1\nABL1\nPILRB\nAXIN1\nFBXL8\nMCF2L\nDBNDD1\nIGHMBP2\nWIPF2\nWFS1\nOGFOD2\nMAPK1IP1L\nCOL11A1\nREG3A\nSERPINA1\nMYCBP2\nPIGK\nTCAP\nCRADD\nELK1\nDNAJB2\nZBTB16\nDAZAP1\nMAPKAPK2\nEDRF1\nCRIP1\nUCP3\nAGR2\nP4HA2',
-        },
-      },
-      {
-        label: 'Example Up and Down Sets',
-        input: {
-          type: 'Rank',
-          up_geneset: 'UTP14A\nS100A6\nSCAND1\nRRP12\nCIAPIN1\nADH5\nMTERF3\nSPR\nCHMP4A\nUFM1\nVAT1\nHACD3\nRFC5\nCOTL1\nNPRL2\nTRIB3\nPCCB\nTLE1\nCD58\nBACE2\nKDM3A\nTARBP1\nRNH1\nCHAC1\nMBNL2\nVDAC1\nTES\nOXA1L\nNOP56\nHAT1\nCPNE3\nDNMT1\nARHGAP1\nVPS28\nEIF2S2\nBAG3\nCDCA4\nNPDC1\nRPS6KA1\nFIS1\nSYPL1\nSARS\nCDC45\nCANT1\nHERPUD1\nSORBS3\nMRPS2\nTOR1A\nTNIP1\nSLC25A46',
-          down_geneset: 'MAL\nEPCAM\nHDAC6\nCAPN1\nTNRC6B\nPKD1\nRRS1\nHP\nANO10\nCEP170B\nIDE\nDENND2D\nCAMK2B\nZNF358\nRPP38\nMRPL19\nNUCB2\nGNAI1\nLSR\nADGRE2\nPKMYT1\nCDK5R1\nABL1\nPILRB\nAXIN1\nFBXL8\nMCF2L\nDBNDD1\nIGHMBP2\nWIPF2\nWFS1\nOGFOD2\nMAPK1IP1L\nCOL11A1\nREG3A\nSERPINA1\nMYCBP2\nPIGK\nTCAP\nCRADD\nELK1\nDNAJB2\nZBTB16\nDAZAP1\nMAPKAPK2\nEDRF1\nCRIP1\nUCP3\nAGR2\nP4HA2',
-        },
-      },
-    ],
-    geneset_switch: 'Gene Set or Full Signature',
-    up_down_switch: 'Up and Down Gene Sets',
-    entity_strategy: 'upper', // lower, none
-    synonym_strategy: 'none',
     resource_order: {},
-    about: "Signature Commons",
-    terms: "",
-    analytics: "UA-6277639-36"
+    about: "Improved methodologies for data collection has accelerated the generation of data in many fields. This shifted the challenge from data generation to ensuring that the data generated are Findable, Accessible, Interoperable, and Reusable (FAIR). Signature Commons (SigCom) is a cloud-agnostic open-source platform designed for storing a wide array of biomedical data. This versatility of SigCom when it comes to data is rooted from the strategy of storing metadata elements as serialized, semi-structured JSON entries that can be paired with unordered or ranked sets of entities such as genes, proteins, and drugs. Signature Commons is composed of a couple of microservices that communicate with each other to provide the users with a seamless experience. The metadata API provides fast full-text search and field comparison filtering of the metadata as well as metadata aggregations for statistical summaries. Furthermore, JSON-Schema validation is performed before ingestion to maintain the integrity of the database and ensure harmonizing the terms with existing ontologies. The data API, on the other hand, provides real-time set-, and two-sided ranked set-enrichment analysis. SigCom includes OpenAPI documentation for these two microservices. A companion web application uses these APIs to provide a data portal for querying, browsing, downloading, and visualizing data. Interactive visualizations include various charts and word clouds to gain insights into the database composition as well as running sum plots for ranked set enrichment. This user interface can be customized by uploading ui schemas to the database, making it easy to deploy portals with different look and feel. Signature Commons provides these microservices as Docker images for a seamless cloud deployment. Signature Commons has been active for over a year and its reusability and ease of deployment has enabled several projects to adopt the platform in building web portals of their own.",
   },
 }
 
-// from deepmerge
-const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray
+export const defaultTheme = {
+  palette: {
+    primary: {
+      main: blue[500],
+      contrastText: '#FFF',
+    },
+    secondary: {
+      main: yellow[900],
+    },
+    error: {
+      main: red[500],
+    },
+    default: {
+      main: grey[200],
+    },
+    defaultCard: {
+      main: grey[400],
+      contrastText: "#FFF",
+    },
+    defaultButton: {
+      disabled: grey[300],
+      main: blue[500],
+    },
+    defaultChip: {
+      main: grey[500],
+      contrastText: "#FFF"
+    },
+    defaultChipLight: {
+      main: grey[300],
+    },
 
-export const UIValues = {
-  'landing': (values) => {
-    const ui_values = merge(default_values['landing'], values, { arrayMerge: overwriteMerge })
-    if (ui_values.overlap_search === false && ui_values.rank_search === false) {
-      ui_values.nav.SignatureSearch.active = false
-    }
-    if (ui_values.background_props.style && ui_values.background_props.style.backgroundImage) {
-      ui_values.background_props.style.backgroundImage = 'url(' + makeTemplate(ui_values.background_props.style.backgroundImage, {}) + ')'
-    }
-    return (ui_values)
   },
-  'admin': (values) => {
-    return (
-      {
-        LandingText: {
-          header: 'Signature Commons',
-          resource_pie_caption: 'Signatures per Resource',
-        },
-        preferred_name_singular: {
-          libraries: 'Dataset',
-          signatures: 'Signature',
-          entities: 'Gene',
-          resources: 'Resource',
-        },
-        preferred_name: {
-          libraries: 'Datasets',
-          signatures: 'Signatures',
-          entities: 'Genes',
-          resources: 'Resources',
-        },
-        bar_chart: values.bar_chart,
-        bar_chart_style: {
-          ResponsiveContainer: {
-            width: '100%',
-            height: 350,
-          },
-          Chart: {
-            margin: {
-              right: 50,
-              left: 50,
-              top: 20,
-              bottom: 20,
-            },
-          },
-          XAxis: {
-            axisLine: false,
-            angle: 45,
-            height: 50,
-            textAnchor: 'start',
-            interval: 0,
-            tick: {
-              fontSize: 10,
-            },
-            tickLine: false,
-          },
-          Bar: {
-            fill: '#75bef5',
-          },
-        },
-        pie_chart_style: {
-          ResponsiveContainer: {
-            width: '100%',
-            height: 350,
-          },
-          Chart: {
-            width: 420,
-            height: 420,
-          },
-          Pie: {
-            fill: '#75bef5',
-          },
-          Text_Label: {
-            fontSize: 10,
-          },
-        },
-        pie_caption: 'Signatures per',
-        entity_name: values.entity_name,
-        entity_synonyms: values.entity_synonyms,
-        counting_validator: '/dcic/signature-commons-schema/v5/meta/schema/counting.json',
-      }
-    )
+  card: {
+    bottomCard: {
+      palette: {},
+      overrides: {},
+    },
+    topCard: {
+      palette: {},
+      overrides: {},
+    },
+  },
+  chipColors: {
+    warning: { backgroundColor: amber[300], color: '#000' },
+    alert: { backgroundColor: red[300], color: '#000' },
+    healthy: { backgroundColor: green[300], color: '#000' },
+  },
+  overrides: {
+    MUIDataTable: {
+      responsiveScroll: {
+        maxHeight: '500px',
+        minHeight: '500px',
+      },
+    },
+    MUIDataTableHeadCell: {
+      root: {
+        fontSize: 13,
+      },
+    },
+    MuiChip: {
+      root: {
+        margin: '5px 10px 5px 0',
+      },
+    },
   },
 }
