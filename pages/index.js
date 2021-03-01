e3a5dd9a46d3f4b4ac2db5c13941969a1e826ee1
erol <je.evangelista04@gmail.com>
2021-03-01 13:22:11 -0500

    simplified entry page

--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,43 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 import CircularProgress from '@material-ui/core/CircularProgress'
-
-import { fetch_meta_post } from '../util/fetch/meta'
-import { UIValues } from '../util/ui/ui_values'
-import { initializeSigcom } from '../util/redux/actions'
-import { connect } from 'react-redux'
+import { get_initial_props } from '../util/ui/fetch_ui_props'
+import { MuiThemeProvider } from '@material-ui/core'
 
 const Router = dynamic(async () => (await import('react-router-dom')).HashRouter, { ssr: false })
 const Route = dynamic(async () => (await import('react-router-dom')).Route, { ssr: false })
-const ArbitraryQuery = dynamic(() => import('../components/ArbitraryQuery'), { ssr: false })
-const DBCK = dynamic(() => import('../components/DBCK'), { ssr: false })
-const EntityPage = dynamic(() => import('../components/EntityPage'), { ssr: false })
 const Home = dynamic(() => import('../components/Home'), { ssr: false })
-const Stats = dynamic(() => import('../components/Stats'), { ssr: false })
-const TermQuery = dynamic(() => import('../components/TermQuery'), { ssr: false })
-const Tests = dynamic(() => import('../components/Tests'), { ssr: false })
-const Values = dynamic(() => import('../components/Values'), { ssr: false })
-
-
-function mapDispatchToProps(dispatch) {
-  return {
-    initializeSigcom: (serverSideProps) => dispatch(initializeSigcom(serverSideProps)),
-  }
-}
-
-
-const mapStateToProps = (state) => {
-  return {
-    initialized: state.initialized,
-  }
-}
-
-export async function get_ui_values() {
-  const { response: ui_val } = await fetch_meta_post({
-    endpoint: '/schemas/find',
-    body: {
-      filter: {
-        where: {
-          'meta.$validator': '/dcic/signature-commons-schema/v5/meta/schema/landing-ui.json',
-          'meta.landing': true,
-        },
-      },
-    },
-  })
-  const values = ui_val.length > 0 ? ui_val[0].meta.content : {}
-  const ui_values = UIValues['landing'](values)
-  return { ui_values }
-}
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      theme: null,
+      ui_values: null,
+    }
+  }
   static async getInitialProps() {
 
   }
 
-  async componentDidMount() {
-    this.props.initializeSigcom()
+  componentDidMount = async () => {
+    const { ui_values, theme_mod } = await get_initial_props()
   }
 
   render() {
-    if (!this.props.initialized) {
+    if (!this.state.theme === null) {
       return <div style={{ textAlign: 'center', marginTop: 100 }}>
         <CircularProgress />
       </div>
     }
     return (
-      <div className="root">
+      <MuiThemeProvider theme={this.props.theme}>
         <Router>
           <div className="root">
-            <Route path="/" render={(router_props) => <Home {...router_props}/>} />
-            {/* <Route exact path="/arbitrary-query" component={ArbitraryQuery} />
-            <Route exact path="/dbck" component={DBCK} />
-            <Route exact path="/entity-page" component={EntityPage} />
-            <Route exact path="/stats" component={Stats} />
-            <Route exact path="/term-query" component={TermQuery} />
-            <Route exact path="/values" component={Values} />
-            <Route exact path="/tests" component={Tests} /> */}
+            <Route path="/" render={(props) => <Home {...props}/>} />
           </div>
         </Router>
-      </div>
+      </MuiThemeProvider>
     )
   }
 }
