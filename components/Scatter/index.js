1eb4413592635610f09b4e99cb62f763c11bdf65
erol <je.evangelista04@gmail.com>
2021-04-28 19:36:07 -0400

    cleanup

--- components/Scatter/index.js
+++ components/Scatter/index.js
@@ -1,157 +1,11 @@
-import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types'
-import {process_data} from './process_data'
 import {ScatterPlot} from './ScatterPlot'
 import {AutoCompleteSelection} from '../AutoCompleteSelection'
 import Grid from '@material-ui/core/Grid'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { RadioButtons } from '../RadioButtons'
 
-const get_top_value_count = async ({resolver, library, field, inputTerm=''}) => {
-	try {
-		resolver.abort_controller()
-		resolver.controller()
-		if (inputTerm === '') {
-			const endpoint =  `/libraries/${library}/signatures/value_count`
-			const filter = {
-				limit: 10,
-				fields: [field],
-			}
-			const value_count = await resolver.aggregate( endpoint, filter)
-			return value_count[field]
-		} else {
-			const endpoint =  `/signatures/value_count`
-			const filter = {
-				where: {
-					library,
-					[field]: {
-						ilike: `%${inputTerm}%`
-					}
-				},
-				limit: 10,
-				fields: [field]
-			}
-			const value_count = await resolver.aggregate( endpoint, filter)
-			return value_count[field]
-		}	
-	} catch (error) {
-		console.error(error)
-	}
-
-}
-
-const filter_metadata = async ({resolver, library, term='', primary_field}) => {
-	try {
-		resolver.abort_controller()
-		resolver.controller()
-		if (term === '') return {}
-		else {
-			const field = primary_field.search_field || primary_field.field
-			const filter = {
-				where: {
-					library: library.id,
-				}
-			}
-			if (term !== "") filter.where[field] = term
-			// if (ids.length > 0) filter.where.id = {inq: ids}
-			const {entries} = await resolver.filter_metadata({
-				model: "signatures",
-				filter,
-				parent: library
-			})
-			return entries
-		}
-	} catch (error) {
-		console.error(error)
-	}
-}
-
-const get_results = async ({resolver, input, datatype, database, entries}) => {
-	try {
-		resolver.abort_controller()
-		resolver.controller()
-		const query = {
-			...input,
-			datatype,
-			database,
-			input_type: input.entities !== undefined ? 'set': 'up_down',
-			signatures: Object.keys(entries)
-		}
-		if (query.signatures.length === 0) query.limit = 100
-		else query.limit = query.signatures.length
-		const {set, up, down, rank} = await resolver.enrich_entities(query)
-		
-		if (set !== undefined) {
-			const { entries: results, count } = set
-
-			if (count>0){
-				const {resolved_entries} = await resolver.resolve_entries({
-					model: "signatures",
-					entries: results.map(({id, direction, ...scores})=>({
-						id,
-						direction,
-						scores
-					}))
-				})
-				return resolved_entries
-			}
-		}if (rank !== undefined) {
-			const { entries: results, count } = rank
-
-			if (count>0){
-				const {resolved_entries} = await resolver.resolve_entries({
-					model: "signatures",
-					entries: results.map(({id, direction, ...scores})=>({
-						id,
-						direction,
-						scores
-					}))
-				})
-				return resolved_entries
-			}
-		}else if (up !== undefined && down !== undefined) {
-			const signatures = {}
-			const { entries: up_entries, up_count } = up
-
-			if (up_count>0){
-				const {resolved_entries} = await resolver.resolve_entries({
-					model: "signatures",
-					entries: up_entries.map(({id, direction, ...scores})=>({
-						id,
-						direction,
-						scores
-					}))
-				})
-				for (const sig of Object.values(resolved_entries)){
-					sig.update_entry({set: "up"})
-					signatures[sig.id] = sig
-				}
-			}
-			const { entries: down_entries, down_count } = down
-
-			if (down_count>0){
-				const {resolved_entries} = await resolver.resolve_entries({
-					model: "signatures",
-					entries: down_entries.map(({id, direction, ...scores})=>({
-						id,
-						direction,
-						scores
-					}))
-				})
-				for (const sig of Object.values(resolved_entries)){
-					sig.update_entry({set: "down"})
-					signatures[sig.id] = sig
-				}
-			}
-			return signatures
-		}
-
-	} catch (error) {
-		console.error(error)
-	}
-}
-
-export default function SignatureScatterPlot(props) {
+const SignatureScatterPlot = (props) => {
 	const { results,
 		name_props,
 		primary_field,
@@ -163,69 +17,6 @@ export default function SignatureScatterPlot(props) {
 		set_term,
 		term,
 	} = props
-	// const database = library.dataset
-	// const datatype = library.dataset_type
-	// const [filter_counts, setFilterCounts] = useState({});
-	// const [term, setTerm] = useState(null);
-	// const [inputTerm, setInputTerm] = useState('');
-	// const [category, setCategory] = useState(null);
-
-	// useEffect( async() => {
-	// 	setFilterCounts({});
-	// 	if (primary_field !== undefined){
-	// 		const results = await get_top_value_count({
-	// 			inputTerm,
-	// 			resolver,
-	// 			library: library.id,
-	// 			field: primary_field.field,
-	// 		}) 
-	// 		setFilterCounts(results);
-	// 		if (term===null) setTerm('')
-	// 	}
-	// }, [inputTerm]);
-
-	// const [entries, setEntries] = useState(null);
-	// useEffect(async () => {
-	// 	if (term !== null){
-	// 		if (primary_field === undefined) setEntries({})
-	// 		else {
-	// 			const entries = await filter_metadata({resolver,
-	// 				resolver,
-	// 				library,
-	// 				term,
-	// 				primary_field
-	// 			})
-	// 			setEntries(entries)
-	// 		}
-	// 	}
-	// }, [term]);
-
-	// const [results, setResults] = useState(null);
-	// useEffect(async () => {
-	// 	if (entries !== null){
-	// 		const signatures = await get_results({
-	// 			resolver,
-	// 			input,
-	// 			datatype,
-	// 			database,
-	// 			entries,
-	// 		})
-	// 		if (signatures !==undefined){
-	// 			const {scatter_data, colorize} = await process_data({
-	// 				entries: signatures,
-	// 				library,
-	// 				schemas,
-	// 				primary_color: primary_color,
-	// 				secondary_color: secondary_color
-	// 			})
-	// 			if (category === null){
-	// 				const cat = colorize.direction !== undefined ? 'direction': 'significance'
-	// 				setCategory(cat)
-	// 			}
-	// 			setResults({scatter_data, colorize})
-	// 		}
-	// 	}
-	// }, [entries]);
 	if (results === null || scatter_value_count === null) return ( 
 		<Grid container style={{height:650, width: "100%"}}>
 			<Grid item xs={12} align="center" style={{margin: 100}}>
@@ -288,3 +79,5 @@ SignatureScatterPlot.propTypes = {
 		}),
 	])
 }
+
+export default SignatureScatterPlot;
