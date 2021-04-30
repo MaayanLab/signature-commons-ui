dc61b6fd948c5a92da42f17c20597b346bd30f17
erol <je.evangelista04@gmail.com>
2021-04-30 11:35:07 -0400

    Made infocard title tex smaller

--- components/DataTable/InfoCard.js
+++ components/DataTable/InfoCard.js
@@ -57,7 +57,7 @@ export const InfoCard = ({
                       <Highlight
                         Component={(props) => {
                           return (
-                            <Typography variant="h5" {...props}>
+                            <Typography variant="h6" {...props}>
                               <a href={info.endpoint}>{props.children}</a>
                             </Typography>
                           )
