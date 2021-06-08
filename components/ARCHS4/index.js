43a8467f99305224d0581074a72e535fd6277704
erol <je.evangelista04@gmail.com>
2021-06-08 17:04:24 -0400

    Fixed slow autocomplete with virtualization

--- components/ARCHS4/index.js
+++ components/ARCHS4/index.js
@@ -1,15 +1,101 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types'
 import { withRouter } from "react-router";
 import {enrich_gene_coexpression, get_gene_names} from './util'
 import dynamic from 'next/dynamic'
-import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
+import { VariableSizeList } from 'react-window';
 
 const Grid = dynamic(()=>import('@material-ui/core/Grid'))
 const Typography = dynamic(()=>import('@material-ui/core/Typography'))
 const TextField = dynamic(()=>import('@material-ui/core/TextField'))
 const Button = dynamic(()=>import('@material-ui/core/Button'))
 const Autocomplete = dynamic(()=>import('@material-ui/lab/Autocomplete'))
+
+// From Material UI
+const LISTBOX_PADDING = 8; // px
+
+function renderRow(props) {
+  const { data, index, style } = props;
+  return React.cloneElement(data[index], {
+    style: {
+      ...style,
+      top: style.top + LISTBOX_PADDING,
+    },
+  });
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data) {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+// Adapter for react-window
+const ListboxComponent = React.forwardRef(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData = React.Children.toArray(children.slice(0,200));
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up('sm'), { noSsr: true });
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 36 : 48;
+
+  const getChildSize = (child) => {
+    if (React.isValidElement(child) && child.type === ListSubheader) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
+ListboxComponent.propTypes = {
+  children: PropTypes.node,
+};
+
+// End
 
 export const GeneSearch = (props) => {
 	const {
@@ -65,6 +151,7 @@ export const GeneSearch = (props) => {
 			<Grid item xs={12}>
 				<Autocomplete
 					options={options}
+					ListboxComponent={ListboxComponent}
 					renderInput={(params) => (
 						<React.Fragment>
 							<TextField 
