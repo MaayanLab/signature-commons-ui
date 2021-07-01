ff461a95d27057d04d68c22817a3676880f002c1
erol <je.evangelista04@gmail.com>
2021-07-01 19:47:53 -0400

    download cards -> tables

--- components/DownloadsPage/DownloadLinks.js
+++ components/DownloadsPage/DownloadLinks.js
@@ -2,16 +2,64 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 const Grid = dynamic(()=> import('@material-ui/core/Grid'));
 const Card = dynamic(()=> import('@material-ui/core/Card'));
-const CardActions = dynamic(()=> import('@material-ui/core/CardActions'));
 const CardHeader = dynamic(()=> import('@material-ui/core/CardHeader'));
 const Button = dynamic(()=> import('@material-ui/core/Button'));
 const Avatar = dynamic(()=> import('@material-ui/core/Avatar'));
 const Typography = dynamic(()=> import('@material-ui/core/Typography'));
 
+const Table = dynamic(()=>import('@material-ui/core/Table'));
+const TableBody = dynamic(()=>import('@material-ui/core/TableBody'));
+const TableCell = dynamic(()=>import('@material-ui/core/TableCell'));
+const TableContainer = dynamic(()=>import('@material-ui/core/TableContainer'));
+const TableHead = dynamic(()=>import('@material-ui/core/TableHead'));
+const TableRow = dynamic(()=>import('@material-ui/core/TableRow'));
+const Paper = dynamic(()=>import('@material-ui/core/Paper'));
+
+
+const DownloadTable = ({links}) => {
+	const values = links.map(({label, icon, href, ...rest})=>({
+		icon: () => (
+			<Avatar aria-label={label}>
+				<span className={`mdi mdi-18px ${icon}`}/>
+			</Avatar>
+		),
+		label,
+		...rest,
+		download: () => (
+			<Button aria-label="add to favorites" target="_blank" rel="noopener noreferrer" href={href} style={{fontSize: 12}}>
+				<span className="mdi mdi-24px mdi-download"/> 
+			</Button>
+		)
+	}))
+	const headers = Object.keys(values[0])
+
+	return (
+		<Table aria-label="download table" size="small">
+			<TableHead>
+				<TableRow>
+					{headers.map(h=>(
+					<TableCell><b>{h === "icon" ? "": h}</b></TableCell>	  
+					))}
+				</TableRow>
+			</TableHead>
+			<TableBody>
+				{values.map((row) => (
+				<TableRow key={row.label}>
+					{headers.map(h=>(
+					<TableCell>{h === "icon" || h === "download" ? row[h]() :row[h]}</TableCell>	  
+					))}
+				</TableRow>
+				))}
+			</TableBody>
+		</Table>
+	)
+}
+
 const DownloadCards = ({links}) => {
-	const cards = links.map(({label, icon, href, size, date})=>(
+	console.log(links)
+	const cards = links.map(({label, icon, href, ...rest})=>(
 		<Grid item xs={6} md={4} xl={3} key={label}>
-		  <Card style={{minHeight: 150}}>
+		  <Card style={{minHeight: 300}}>
 			<CardHeader
 			  avatar={
 				<Avatar aria-label={label}>
@@ -21,11 +69,12 @@ const DownloadCards = ({links}) => {
 			  title={label}
 			  subheader={
 			  <React.Fragment>
-				  <Typography variant="subtitle2">{`Size: ${size}`}</Typography>
-				  <Typography variant="subtitle2">{`Date: ${date}`}</Typography>
+				  {Object.entries(rest).map(([label, value])=>(
+					<Typography variant="subtitle2">{`${label}: ${value}`}</Typography>
+				  ))}
 			  </React.Fragment>
 			  }
-			  style={{textAlign: "left", minHeight: 150, alignItems: "flex-start"}}
+			  style={{textAlign: "left", minHeight: 300, alignItems: "flex-start"}}
 			  action={
 				<Button aria-label="add to favorites" target="_blank" rel="noopener noreferrer" href={href} style={{fontSize: 12}}>
 					<span className="mdi mdi-24px mdi-download"/> 
@@ -51,7 +100,7 @@ export default function Download( {download_links} ) {
 						<Typography variant={"body2"}>{description}</Typography>
 					</Grid>
 				}
-				<DownloadCards links={links}/>
+				<DownloadTable links={links}/>
 			</React.Fragment>
 		))}
 	  </Grid>
