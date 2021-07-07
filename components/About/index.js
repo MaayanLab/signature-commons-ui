1d817a3503d7929049a20611df446418da03a25e
erol <je.evangelista04@gmail.com>
2021-07-06 20:13:02 -0400

    Added back markdown component + scaling

--- components/About/index.js
+++ components/About/index.js
@@ -15,7 +15,7 @@ const Carousel = dynamic(()=>import('react-material-ui-carousel'));
 
 const DonutChart = dynamic(()=>import('./PieChart'));
 const HorizontalBarChart = dynamic(()=>import('./BarChart'));
-// const MarkdownComponent = dynamic(()=>import('../Markdown/MarkdownComponent'));
+const MarkdownComponent = dynamic(()=>import('../Markdown/MarkdownComponent'));
 const IconComponentButton = dynamic(async () => (await import('../DataTable')).IconComponentButton);
 
 
@@ -51,16 +51,18 @@ class About extends React.PureComponent {
     const { model_counts } = this.state.stats
     const { nav, preferred_name } = this.props.ui_values
     const md = 12/Object.keys(model_counts).length
-    const models = model_counts.map(m=>(
+    const models = model_counts.map(m=>{
+    const url = m.model === "resources" ? `#${nav.Resources.endpoint}`: `#${nav.MetadataSearch.endpoint}/${preferred_name[m.model]}`
+    return(
       <Grid item xs={12} key={m.name}>
         <IconComponentButton
           title={m.count}
           subtitle={m.name}
           icon={m.icon}
-          href={`#${nav.MetadataSearch.endpoint}/${preferred_name[m.model]}`}
+          href={url}
           description={`Explore ${preferred_name[m.model]}`}/>
       </Grid>
-    ))
+    )})
     return (
       <Grid container direction="column" align="center"> 
         {models}
@@ -68,15 +70,26 @@ class About extends React.PureComponent {
     )
   }
 
-  charts = () => {
+  charts = ({ignore_bar, ignore_pie, scale_bar=[], scale_pie=[]}) => {
+    const ignore = {
+      pie: ignore_pie || {},
+      bar: ignore_bar || {},
+    }
     const { pie, bar } = this.state.stats.count_charts
     const {scores = {}} = this.state.stats
     const charts = [...Object.values(pie), ...Object.values(bar), ...Object.values(scores)].sort((a,b)=>a.priority-b.priority)
     const { nav, preferred_name } = this.props.ui_values
     return charts.map(k => {
       const type = k.type || 'bar'
-      const data = k.stats.filter(a=>a.count>0).sort((a,b)=>(b.count-a.count))
+      let data = k.stats.filter(a=>a.count>0 && (ignore[type][k.name] || []).indexOf(a.name) === -1).sort((a,b)=>(b.count-a.count))
       if (type === 'pie'){
+        if (scale_pie.indexOf(k.name)>-1) {
+          data = data.map(d=>({
+            ...d,
+            count: Math.log(d.count),
+            unscaled_count: d.count
+          }))
+        }
         return(
           <Box key={k.name}>
             <DonutChart
@@ -98,7 +111,7 @@ class About extends React.PureComponent {
           <Box key={k.name}>
             <HorizontalBarChart
               data={data}
-              pie_chart_style={{Pie: {fill: this.props.theme.palette.primaryVisualization.main}}}
+              scale={scale_bar.indexOf(k.name)>-1 ? "log": "auto"}//{k.scale || "auto"}
               color={this.props.theme.palette.primaryVisualization.main}
               onClick={(data)=>{
                 const search = data.payload.name
@@ -118,20 +131,23 @@ class About extends React.PureComponent {
 
   render() {
     if (this.state === null) return <CircularProgress/>
+    const {about, ignore_bar, ignore_pie, scale_bar, scale_pie} = this.props.ui_values.nav.About.props
     return (
       <Grid container spacing={1} style={{marginBottom: 50}}>
-          {/* <Grid item xs={12} lg={6}>
-            <Typography align="justify">
-              <MarkdownComponent url={this.props.ui_values.nav.About.props.about} />
-            </Typography>
-          </Grid> */}
+          {this.props.location.pathname === this.props.ui_values.nav.About.endpoint &&
+            <Grid item xs={12}>
+              <Typography align="justify">
+                <MarkdownComponent url={about} />
+              </Typography>
+            </Grid>
+          }
 
           <Grid item xs={2} align="right">
             {this.model_counts()}
           </Grid>
           <Grid item xs={10} align="center">
             <Carousel>
-              {this.charts()}
+              {this.charts({ignore_bar, ignore_pie, scale_bar, scale_pie})}
             </Carousel>
           </Grid>
       </Grid>
